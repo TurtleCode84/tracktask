d368bfd84810c87f1489d265ffb3459b9d13a0f4
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-03 12:35:20 -0400

    Update login.js

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   if (req.method === 'POST') {
-    res.status(404).json({ message: 'Endpoint coming soon!' })
+    res.status(200).json({ message: 'Endpoint coming soon!' })
   } else {
-    res.status(404).json({ message: 'Try POSTing data instead!' })
+    res.status(405)
   }
 }
