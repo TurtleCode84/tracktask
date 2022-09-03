e9101eddae4ed61bf9cf4172d522570ad57eaf35
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-03 18:24:38 -0400

    Update mongodb.js

--- util/mongodb.js
+++ util/mongodb.js
@@ -14,7 +14,6 @@ if (!process.env.MONGODB_URI) {
 }
 
 if (process.env.NODE_ENV === 'development') {
-Replacement).
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options)
     global._mongoClientPromise = client.connect()
