7f9b727f7314624cfb79cf6c29645b2ffcf14643
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-03 18:18:26 -0400

    Update [user].js

--- pages/api/users/[user].js
+++ pages/api/users/[user].js
@@ -1,4 +1,4 @@
-import clientPromise from "../../../util/mongodb";
+import clientPromise from "../../../util/mongodb.js";
 
 export default async function handler(req, res) {
   const client = await clientPromise;
