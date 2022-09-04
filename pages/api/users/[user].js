ddb4f2846bc6341321b3715dc41d04b05ff69602
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-03 21:33:49 -0400

    Update [user].js

--- pages/api/users/[user].js
+++ pages/api/users/[user].js
@@ -4,9 +4,9 @@ export default async function handler(req, res) {
   const { user } = req.query
   const client = await clientPromise;
   const db = client.db("data");
-  const userInfo = await db.collection("users").find({ username : user });
-  if (userInfo.count() !== 0) {
-    userInfo = userInfo.toArray();
+  const userObject = await db.collection("users").find({ username : user });
+  if (userObject.count() !== 0) {
+    const userInfo = userObject.toArray();
     res.json({ user : userInfo });
   } else {
     res.status(404).json({ error : "user not found" });
