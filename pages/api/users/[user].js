4876b9634803e56b8a0de8f4fafe495f19eb5596
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-03 21:09:24 -0400

    Update [user].js

--- pages/api/users/[user].js
+++ pages/api/users/[user].js
@@ -1,7 +1,7 @@
 import clientPromise from "../../../util/mongodb";
 
 export default async function handler(req, res) {
-  const user = req.query;
+  const { user } = req.query
   const client = await clientPromise;
   const db = client.db("data");
   const userInfo = await db.collection("users").find({ username : user }).toArray();
