30998bc804f6c7344dcd136ce6d4db7446f9f5e7
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-05 09:32:27 -0400

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,1 +1,28 @@
+import withSession from '../util/session'
 
+export const getServerSideProps = withSession(async function ({ req, res }) {
+  const { user } = req.session
+
+  if (!user) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: { user },
+  }
+})
+
+const Dashboard = ({ user }) => {
+  // Show the user. No loading state is required
+  return (
+    <h1>Your Profile</h1>
+    <pre>{JSON.stringify(user, null, 2)}</pre>
+  )
+}
+
+export default Dashboard
