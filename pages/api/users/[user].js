a0f7789dacac8050b6bf50798922a3987576b86d
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-06 17:26:00 -0400

    Update [user].js

--- pages/api/users/[user].js
+++ pages/api/users/[user].js
@@ -3,16 +3,19 @@ import { ObjectId } from "mongodb";
 
 export default async function handler(req, res) {
   const { user } = req.query
-  const client = await clientPromise;
-  const db = client.db("data");
-  const query = { _id: ObjectId(user) };
-  //const exists = await db.collection("users").find({ _id : ObjectId(user) }).count();
-  const exists = await db.collection("users").countDocuments(query);
-  if (exists > 0) {
-    const projection = { username: 1, "history.joined": 1, permissions: 1, bio: 1, profilePicture: 1 };
-    const userInfo = await db.collection("users").find(query).project(projection).toArray(); // yes, I know it's a bit inefficient, but nothing else was working
-    res.json(userInfo);
+  if(ObjectId.isValid(task)) {
+    const client = await clientPromise;
+    const db = client.db("data");
+    const query = { _id: ObjectId(user) };
+    const exists = await db.collection("users").countDocuments(query);
+    if (exists > 0) {
+      const projection = { username: 1, "history.joined": 1, permissions: 1, bio: 1, profilePicture: 1 };
+      const userInfo = await db.collection("users").find(query).project(projection).toArray(); // yes, I know it's a bit inefficient, but nothing else was working
+      res.json(userInfo);
+    } else {
+      res.status(404).json({ error : "user not found" });
+    }
   } else {
-    res.status(404).json({ error : "user not found" });
+    res.status(500).json({ error : "invalid user id" });
   }
 }
