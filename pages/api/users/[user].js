4e60d12e7deb614e5d27d6951e0909aad766dda1
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-06 15:30:33 -0400

    Update and rename [user] to [user].js

--- pages/api/users/[user].js
+++ pages/api/users/[user].js
@@ -6,8 +6,8 @@ export default async function handler(req, res) {
   const db = client.db("data");
   const exists = await db.collection("users").find({ username : user }).count();
   if (exists !== 0) {
-    const userInfo = await db.collection("users").find({ username : user }).toArray(); // yes, I know it's very inefficient, but nothing else was working
-    res.json({ user : userInfo });
+    const userInfo = await db.collection("users").find({ username : user }).toArray(); // yes, I know it's a bit inefficient, but nothing else was working
+    res.json({ userInfo });
   } else {
     res.status(404).json({ error : "user not found" });
   }
