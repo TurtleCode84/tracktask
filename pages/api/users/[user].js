445187cc30eaa70a5bee8df06cc0630ab6c1b41d
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-06 15:50:47 -0400

    Update [user].js

--- pages/api/users/[user].js
+++ pages/api/users/[user].js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   const db = client.db("data");
   const exists = await db.collection("users").find({ username : user }).count();
   if (exists !== 0) {
-    const userInfo = await db.collection("users").find({ username : user }, { username: 1, permissions: 1, "history.joined": 1, bio: 1, profilePicture: 1 }).toArray(); // yes, I know it's a bit inefficient, but nothing else was working
+    const userInfo = await db.collection("users").find({ username : user }, { email: 0, password: 0, shareKey: 0, history: 0, "history.joined": 1 }).toArray(); // yes, I know it's a bit inefficient, but nothing else was working
     res.json(userInfo);
   } else {
     res.status(404).json({ error : "user not found" });
