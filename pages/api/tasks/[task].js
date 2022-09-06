f9b638dc50d9f565c4942d09f706e3a204311a24
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-06 16:23:51 -0400

    Update [task].js

--- pages/api/tasks/[task].js
+++ pages/api/tasks/[task].js
@@ -4,12 +4,12 @@ export default async function handler(req, res) {
   const { task } = req.query
   const client = await clientPromise;
   const db = client.db("data");
-  const exists = await db.collection("tasks").find({ _id : task, hidden : false }).count();
+  const exists = await db.collection("tasks").find({ _id : ObjectId(task), hidden : false }).count();
   if (exists !== 0) {
     const projection = { name: 1, description: 1, dueDate: 1, owner: 1, completion: 1 };
-    const userInfo = await db.collection("tasks").find({ _id : task }).project(projection).toArray(); // yes, I know it's a bit inefficient, but nothing else was working
+    const userInfo = await db.collection("tasks").find({ _id : ObjectId(task) }).project(projection).toArray(); // yes, I know it's a bit inefficient, but nothing else was working
     res.json(userInfo);
   } else {
-    res.status(404).json({ error : "user not found" });
+    res.status(404).json({ error : "task not found" });
   }
 }
