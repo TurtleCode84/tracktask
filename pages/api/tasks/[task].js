969a3390e5fed693357ac00afd75f2a56774b735
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-06 16:46:34 -0400

    Update [task].js

--- pages/api/tasks/[task].js
+++ pages/api/tasks/[task].js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   const { task } = req.query
   const client = await clientPromise;
   const db = client.db("data");
-  const exists = await db.collection("tasks").find({ _id : task, hidden : false }).count();
+  const exists = await db.collection("tasks").find({ _id : task }).count();
   if (exists !== 0) {
     const projection = { name: 1, description: 1, dueDate: 1, owner: 1, completion: 1 };
     const userInfo = await db.collection("tasks").find({ _id : task }).project(projection).toArray(); // yes, I know it's a bit inefficient, but nothing else was working
