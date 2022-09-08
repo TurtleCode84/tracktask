5cc3db7d59d61dd9377fb06dddc0cb5318dc2127
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-08 19:47:54 -0400

    Create auth.js

---
+++ pages/api/users/auth.js
@@ -0,0 +1,67 @@
+import clientPromise from "../../../lib/mongodb";
+import { hash } from 'bcryptjs';
+//import { NextRequest } from 'next/server'; // unneeded?
+
+async function handler(req, res) {
+  //Only POST mothod is accepted
+  if (req.method === 'POST') {
+    //Getting email and password from body
+    const { username, password } = req.body;
+    //Validate
+    if (!username || !password) {
+      res.status(422).json({ error: 'invalid data' });
+      return;
+    }
+    //Connect with database
+    const client = await clientPromise;
+    const db = client.db("data");
+    //Check existing user
+    const passwordHashed = await hash(password, 10);
+    const query = { username: username.toLowerCase(), passwordHashed };
+    const userExists = await db.collection("users").countDocuments(query);
+    if (userExists < 1) {
+      res.status(422).json({ error: 'incorrect username or password' });
+      return;
+    }
+    
+    //Get the rest of the user info
+    //const projection = { username: 1, "history.joined": 1, permissions: 1, bio: 1, profilePicture: 1 };
+    const userInfo = await db.collection("users").find(query)/*.project(projection)*/;
+    
+    //Get user IP
+    /*const ipList = req.headers["x-forwarded-for"].split(',');
+    const ip = ipList[ipList.length-1];*/
+    const ip = req.headers["x-forwarded-for"].split(',')[0];
+    /*if (req.headers["x-forwarded-for"]) {
+      const ip = req.headers["x-forwarded-for"].split(',')[0];
+    } else if (req.headers["x-real-ip"]) {
+      const ip = req.connection.remoteAddress;
+    } else {
+      const ip = req.connection.remoteAddress;
+    }*/
+    //Formulate and update IP list in database
+    if (userInfo.history.loginIpList[5]) {
+      const temp = userInfo.history.loginIpList.unshift(ip);
+      const newIpList = userInfo.history.loginIpList.pop();
+    } else {
+      const newIpList = userInfo.history.loginIpList.unshift(ip);
+    }
+    const ipUpdateDoc = {
+      $set: {
+        history.loginIpList: newIpList
+      },
+    };
+    const ipUpdate = await db.collection('users').updateOne(query, ipUpdateDoc);
+    const status = await db.collection('sessions').insertOne({
+      userId: userInfo._id,
+      expires: Math.floor((Date.now()/1000) + 3600), // Current timestamp plus 60 minutes
+    });
+    //Send success response
+    res.status(201).json({ message: 'user logged in', ...status });
+  } else {
+    //Response for other than POST method
+    res.status(405).json({ error: 'method not allowed' });
+  }
+}
+
+export default handler;
