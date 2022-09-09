c1d23c7ecf185bbeb875cff5d488aea151075f63
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-09 08:46:58 -0400

    Update auth.js

--- pages/api/users/auth.js
+++ pages/api/users/auth.js
@@ -1,5 +1,5 @@
 import clientPromise from "../../../lib/mongodb";
-import { hash, compare } from 'bcrypt';
+import { compare } from 'bcryptjs';
 //import { NextRequest } from 'next/server'; // unneeded?
 
 async function handler(req, res) {
