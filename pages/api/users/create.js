46fe16dcbd0607eb36868ea554bfa9e527b40e9e
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-09 09:17:03 -0400

    Update create.js

--- pages/api/users/create.js
+++ pages/api/users/create.js
@@ -1,5 +1,5 @@
 import clientPromise from "../../../lib/mongodb";
-import { hash } from 'bcrypt';
+import { hash } from 'bcryptjs';
 //import { NextRequest } from 'next/server'; // unneeded?
 
 async function handler(req, res) {
