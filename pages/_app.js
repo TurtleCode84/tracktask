b095727c76d3165afe36a17982aed0d09d8501bd
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-13 09:11:47 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import { SWRConfig } from "swr";
+import fetchJson from "lib/fetchJson";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SWRConfig
+      value={{
+        fetcher: fetchJson,
+        onError: (err) => {
+          console.error(err);
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
