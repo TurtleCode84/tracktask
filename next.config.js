5a6d1eb3ab5765bcefbcc96504fc594b20b1cf37
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-16 17:55:27 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["avatars.githubusercontent.com"],
+    domains: ["avatars.githubusercontent.com", "u.cubeupload.com", "i.bb.co"],
   },
 };
