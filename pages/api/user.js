72bd9db10e7c228add0e3e224919393699c823cc
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-16 18:42:49 -0400

    Update user.js

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,22 +1,28 @@
 import { withIronSessionApiRoute } from "iron-session/next";
 import { sessionOptions } from "lib/session";
+import clientPromise from "lib/mongodb";
 
 export default withIronSessionApiRoute(userRoute, sessionOptions);
 
 async function userRoute(req, res) {
   if (req.session.user) {
-    // in a real world application you might read the user id from the session and then do a database request
-    // to get more information on the user if needed
+    const query = { _id: req.session.user.id };
+    const userInfo = await db.collection("users").findOne(query);
     res.json({
       ...req.session.user,
       isLoggedIn: true,
+      email: userInfo.email,
+      profilePicture: userInfo.profilePicture,
+      history: { joined: userInfo.history.joined, lastLogin: userInfo.history.lastLogin },
+      shareKey: userInfo.shareKey,
+      bio: userInfo.bio,
+      permissions: userInfo.permissions,
     });
   } else {
     res.json({
       isLoggedIn: false,
-      login: "",
-      avatarUrl: "",
-      followers: "",
+      id: "",
+      username: "",
     });
   }
 }
