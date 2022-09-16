fbfa448f693a058b2fe5074b289c66bf242035fc
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-16 08:46:04 -0400

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -4,18 +4,48 @@ import { sessionOptions } from "lib/session";
 const octokit = new Octokit();
 
 export default withIronSessionApiRoute(async (req, res) => {
-  const { username } = await req.body;
+  if (req.method === 'POST') {
+    const { username, password } = await req.body;
+    
+    //Validate
+    if (!username || !password) {
+      res.status(401).json({ message: "invalid data" });
+      return;
+    }
+    //Connect with database
+    const client = await clientPromise;
+    const db = client.db("data");
+    
+    //Check existing user
+    const query = { username: username.toLowerCase() };
+    const userExists = await db.collection("users").countDocuments(query);
+    if (userExists < 1) {
+      res.status(401).json({ message: "incorrect username or password" }); // user does not exist
+      return;
+    }
+    //Check the password
+    const projection = { password: 1 };
+    const userInfo = await db.collection("users").findOne(query).project(projection);
+    //Check the password
+    const passwordMatch = await compare(password, userInfo.password);
+    if (!passwordMatch) {
+      res.status(401).json({ error: 'incorrect username or password' }); // password is incorrect
+      return;
+    }
+    //Otherwise...
+    try {
+      const {
+        data: { id: _id, username, profilePicture },
+      } = await db.collection("users").findOne(query);
 
-  try {
-    const {
-      data: { login, avatar_url, followers },
-    } = await octokit.rest.users.getByUsername({ username });
-
-    const user = { isLoggedIn: true, login, avatarUrl: avatar_url, followers: followers };
-    req.session.user = user;
-    await req.session.save();
-    res.json(user);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+      const user = { isLoggedIn: true, id, username, profilePicture };
+      req.session.user = user;
+      await req.session.save();
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  } else {
+    res.status(405).json({ message: "method not allowed" });
   }
 }, sessionOptions);
