c192f9845f925acffcad0f52421afc8faf0a547e
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-16 08:47:14 -0400

    Update Form.js

--- components/Form.js
+++ components/Form.js
@@ -2,8 +2,12 @@ export default function Form({ errorMessage, onSubmit }) {
   return (
     <form onSubmit={onSubmit}>
       <label>
-        <span>Type your GitHub username</span>
+        <span>Username</span>
         <input type="text" name="username" required />
+      </label>
+      <label>
+        <span>Password</span>
+        <input type="password" name="password" required />
       </label>
 
       <button type="submit">Login</button>
