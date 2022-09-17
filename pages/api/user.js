8ee6b2b342f59a33b7edfa88ac4b37abd47ceaac
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-17 16:36:28 -0400

    Update user.js

--- pages/api/user.js
+++ pages/api/user.js
@@ -8,14 +8,13 @@ async function userRoute(req, res) {
   if (req.session.user) {
     const client = await clientPromise;
     const db = client.db("data");
-    const query = { _id: req.session.user.id };
-    const options = { projection: { permissions: 1 } };
-    const userInfo = await db.collection("users").findOne(query, options);
+    const query = { _id: ObjectId(req.session.user.id) };
+    //const options = { projection: { permissions: 1 } };
+    const userInfo = await db.collection("users").findOne(query);
     res.json({
       ...req.session.user,
       isLoggedIn: true,
-      //permissions: userInfo.permissions,
-      return: userInfo,
+      permissions: userInfo.permissions,
     });
   } else {
     res.json({
