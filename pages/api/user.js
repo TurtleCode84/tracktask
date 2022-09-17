28338a7c194ecaeec851543801013e3ab381a0e6
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-17 16:30:45 -0400

    Update user.js

--- pages/api/user.js
+++ pages/api/user.js
@@ -6,10 +6,15 @@ export default withIronSessionApiRoute(userRoute, sessionOptions);
 
 async function userRoute(req, res) {
   if (req.session.user) {
-    //get db user stuff
+    const client = await clientPromise;
+    const db = client.db("data");
+    const query = { _id: req.session.user.id };
+    const options = { projection: { permissions: 1 } };
+    const userInfo = await db.collection("users").findOne(query, options);
     res.json({
       ...req.session.user,
       isLoggedIn: true,
+      permissions: userInfo.permissions,
     });
   } else {
     res.json({
