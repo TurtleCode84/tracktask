887f2103357c8346f183c71b8c60dbbeb0b48afb
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-17 19:19:43 -0400

    Update and rename pages/old-admin.js to pages/admin/index.js

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -2,19 +2,29 @@ import React from "react";
 import Layout from "components/Layout";
 import useUser from "lib/useUser";
 
-export default function OldAdmin() {
+export default function Admin() {
   const { user, mutateUser } = useUser({
     redirectTo: "/",
     adminOnly: true,
   });
 
+  if (!user || !user.isLoggedIn || !user.permissions.admin) {
+    return (
+      <Layout>
+        <p>Loading...</p>
+      </Layout>
+    );
+  }
   return (
     <Layout>
       <h1>TrackTask Admin Panel</h1>
       <h2>
         You shouldn&apos;t be here...
       </h2>
-      {user && (
+      <p style={{ fontStyle: "italic" }}>
+        Luckily, there&apos;s not anything here yet.
+      </p>
+    {/*{user && (
         <>
           <p style={{ fontStyle: "italic" }}>
             Your user info, pulled from the TrackTask API.
@@ -22,7 +32,7 @@ export default function OldAdmin() {
 
           <pre>{JSON.stringify(user, null, 2)}</pre>
         </>
-      )}
+      )}*/}
     </Layout>
   );
 }
