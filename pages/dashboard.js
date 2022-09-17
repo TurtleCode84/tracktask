1ad8ddff9f2f7ef25297a634eda19be7cc95f300
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-17 19:06:09 -0400

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -11,7 +11,7 @@ export default function Dashboard() {
   const { events } = useEvents(user);
   {/*const { tasks } = useTasks(user);*/}
   
-  if (!user || user.isLoggedIn || user.permissions.banned) {
+  if (!user || !user.isLoggedIn || user.permissions.banned) {
     return null;
   }
   return (
