08da4e85662504b9c966a3bcb352aba5ddba249a
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-18 15:28:48 -0400

    Update and rename Form.js to LoginForm.js

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,4 +1,4 @@
-export default function Form({ errorMessage, onSubmit }) {
+export default function LoginForm({ errorMessage, onSubmit }) {
   return (
     <form onSubmit={onSubmit}>
       <label>
