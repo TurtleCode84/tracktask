c7811674692a9b9667d34669ce5f3f388909a7a3
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-18 16:45:09 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -31,8 +31,8 @@ export default function Header() {
           )}
           {user?.isLoggedIn === false && (
             <li>
-              <Link href="/signup">
-                <a>Sign up</a>
+              <Link href="/join">
+                <a>Join</a>
               </Link>
             </li>
           )}
