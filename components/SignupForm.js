c72a9b1f100c22c38acbeb439d31166c94af8cc7
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-18 15:34:42 -0400

    Create SignupForm.js

---
+++ components/SignupForm.js
@@ -0,0 +1,47 @@
+export default function SignupForm({ errorMessage, onSubmit }) {
+  return (
+    <form onSubmit={onSubmit}>
+      <label>
+        <span>Username</span>
+        <input type="text" name="username" required />
+      </label>
+      <label>
+        <span>Email (optional)</span>
+        <input type="text" name="email" />
+      </label>
+      <label>
+        <span>Password</span>
+        <input type="password" name="password" required />
+      </label>
+      <label>
+        <span>Confirm password</span>
+        <input type="password" name="cpassword" required />
+      </label>
+
+      <button type="submit">Sign up</button>
+
+      {errorMessage && <p className="error">{errorMessage}</p>}
+
+      <style jsx>{`
+        form,
+        label {
+          display: flex;
+          flex-flow: column;
+        }
+        label > span {
+          font-weight: 600;
+        }
+        input {
+          padding: 8px;
+          margin: 0.3rem 0 1rem;
+          border: 1px solid #ccc;
+          border-radius: 4px;
+        }
+        .error {
+          color: brown;
+          margin: 1rem 0 0;
+        }
+      `}</style>
+    </form>
+  );
+}
