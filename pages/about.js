ce036190196df33b89563bcc4e9947960e7fc1d5
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-18 20:16:54 -0400

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Layout from "components/Layout";
+import Loading from "components/Loading";
+import Image from "next/image";
+{/*import useUser from "lib/useUser";*/}
+
+export default function About() {
+  {/*const { user } = useUser({
+    redirectTo: "/dashboard",
+    redirectIfFound: true,
+  });
+  if (!user || user.isLoggedIn) {
+    return (
+      <Loading/>
+    );
+  }*/}
+  return (
+    <Layout>
+      <h2>About TrackTask:</h2>
+      <p style={{ fontStyle: "italic" }}>
+        Coming soon...
+      </p>
+
+      <style jsx>{`
+        li {
+          margin-bottom: 0.5rem;
+        }
+      `}</style>
+    </Layout>
+  );
+}
