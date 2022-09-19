d472c30d8b7d96f763e397cfb5676678444c7ba6
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-19 09:47:13 -0400

    Update DueDate.js

--- components/DueDate.js
+++ components/DueDate.js
@@ -11,35 +11,35 @@ export default function DueDate({ timestamp }) {
     case true:
       if (delta < 60) { //seconds
         return (
-          <>in {delta} seconds</>
+          <>in {Math.floor(delta)} seconds</>
         );
       } else if (delta < 3600) { //minutes
         return (
-          <>in {delta/60} minutes</>
+          <>in {Math.floor(delta/60)} minutes</>
         );
       } else if (delta < 86400) { //hours
         return (
-          <>in {delta/3600} hours</>
+          <>in {Math.floor(delta/3600)} hours</>
         );
       } else if (delta < 2635200) { //days
         return (
-          <>in {delta/86400} days</>
+          <>in {Math.floor(delta/86400)} days</>
         );
       } else if (delta < 31622400) { //months (approx 30.5 days)
         return (
-          <>in {delta/2635200} months</>
+          <>in {Math.floor(delta/2635200)} months</>
         );
       } else if (delta < 316224000) { //years
         return (
-          <>in {delta/31622400} years</>
+          <>in {Math.floor(delta/31622400)} years</>
         );
-      } else if (delta < 3162240000) { //decades
+      } else if (delta < 3162240000)) { //decades
         return (
-          <>in {delta/31622400} decades</>
+          <>in {Math.floor(delta/31622400)} decades</>
         );
       } else if (delta < 31622400000) { //centuries
         return (
-          <>in {delta/3162240000} centuries</>
+          <>in {Math.floor(delta/3162240000)} centuries</>
         );
       } else { //everything else
         return (
@@ -50,35 +50,35 @@ export default function DueDate({ timestamp }) {
     case false:
       if (delta > -60) { //seconds
         return (
-          <>{delta} seconds ago</>
+          <>{Math.abs(Math.floor(delta))} seconds ago</>
         );
       } else if (delta > -3600) { //minutes
         return (
-          <>{delta/-60} minutes ago</>
+          <>{Math.abs(Math.floor(delta/-60))} minutes ago</>
         );
       } else if (delta > -86400) { //hours
         return (
-          <>{delta/-3600} hours ago</>
+          <>{Math.abs(Math.floor(delta/-3600))} hours ago</>
         );
       } else if (delta > -2635200) { //days
         return (
-          <>{delta/-86400} days ago</>
+          <>{Math.abs(Math.floor(delta/-86400))} days ago</>
         );
       } else if (delta > -31622400) { //months (approx 30.5 days)
         return (
-          <>{delta/-2635200} months ago</>
+          <>{Math.abs(Math.floor(delta/-2635200))} months ago</>
         );
       } else if (delta > -316224000) { //years
         return (
-          <>{delta/-31622400} years ago</>
+          <>{Math.abs(Math.floor(delta/-31622400))} years ago</>
         );
       } else if (delta > -3162240000) { //decades
         return (
-          <>{delta/-31622400} decades ago</>
+          <>{Math.abs(Math.floor(delta/-31622400))} decades ago</>
         );
       } else if (delta > -31622400000) { //centuries
         return (
-          <>{delta/-3162240000} centuries ago</>
+          <>{Math.abs(Math.floor(delta/-3162240000))} centuries ago</>
         );
       } else { //everything else
         return (
