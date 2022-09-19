a4213cbe0aed10325f846203b348186284309f4e
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-18 20:13:29 -0400

    Update index.js

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -21,17 +21,17 @@ export default function Admin() {
         You shouldn&apos;t be here...
       </h2>
       <p style={{ fontStyle: "italic" }}>
-        Luckily, there&apos;s not anything here yet.
+        Luckily, there&apos;s not much here yet.
       </p>
-    {/*{user && (
+    {user && (
         <>
           <p style={{ fontStyle: "italic" }}>
-            Your user info, pulled from the TrackTask API.
+            Current user info, pulled from the TrackTask API.
           </p>
 
           <pre>{JSON.stringify(user, null, 2)}</pre>
         </>
-      )}*/}
+      )}
     </Layout>
   );
 }
