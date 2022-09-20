1348385f7dd9f7da5589521ddcd51d10a5d7200e
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-20 15:58:11 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,15 @@ import fetchJson from "lib/fetchJson";
 
 function MyApp({ Component, pageProps }) {
   return (
+    <GoogleReCaptchaProvider
+      reCaptchaKey="6LdsxRUiAAAAAHKyiqb10cDdj_kWEWQkgpatCWPX"
+      scriptProps={{
+        async: false,
+        defer: false,
+        appendTo: "head",
+        nonce: undefined,
+      }}
+    >
     <SWRConfig
       value={{
         fetcher: fetchJson,
@@ -13,6 +22,7 @@ function MyApp({ Component, pageProps }) {
     >
       <Component {...pageProps} />
     </SWRConfig>
+    </GoogleReCaptchaProvider>
   );
 }
 
