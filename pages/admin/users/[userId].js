0c6b6248952dd8cd47f316c0891c4cf202a34ca4
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-19 20:49:27 -0400

    Update [userId].js

--- pages/admin/users/[userId].js
+++ pages/admin/users/[userId].js
@@ -12,7 +12,7 @@ export default function Admin() {
   });
   const router = useRouter()
   const { userId } = router.query
-  const { data: getUser } = useSWR({ url: '/api/admin/users', args: userId });
+  const { getUser } = useSWR(`/api/admin/users?uid={userId}`);
 
   if (!user || !user.isLoggedIn || !user.permissions.admin) {
     return (
