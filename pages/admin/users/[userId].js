c42914a30cecb7b6742eb82a9636e3a9fa53677b
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-20 19:06:52 -0400

    Update [userId].js

--- pages/admin/users/[userId].js
+++ pages/admin/users/[userId].js
@@ -29,10 +29,10 @@ export default function Admin() {
       </h2>
       <p>User ID: {lookup._id}</p>
       <p>Username: {lookup.user}</p>
-      <p>Email: <Link href={`mailto:${lookup.email}`}>{lookup.email}</Link></p>
+      {/*<p>Email: <Link href={`mailto:${lookup.email}`}>{lookup.email}</Link></p>
       <p title={moment.unix(lookup.joined).format("dddd, MMMM Do YYYY, h:mm:ss a")}>Joined: {moment.unix(lookup.joined).fromNow()}</p>
       <p>Join IP address: <Link title="Lookup IP address" href={`https://whatismyipaddress.com/ip/${lookup.joinedIp}`}>{lookup.joinedIp}</Link></p>
-      <p title={moment.unix(lookup.lastLogin).format("dddd, MMMM Do YYYY, h:mm:ss a")}>Last login: {moment.unix(lookup.lastLogin).fromNow()}</p>
+      <p title={moment.unix(lookup.lastLogin).format("dddd, MMMM Do YYYY, h:mm:ss a")}>Last login: {moment.unix(lookup.lastLogin).fromNow()}</p>*/}
       <p>Admin notes: {lookup.notes}</p>
       <pre>{JSON.stringify(getUser, null, 2)}</pre>
     </Layout>
