250ce4f7bc1fe4379dcf5c7bcf242daf14f5865f
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-20 16:35:55 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 function MyApp({ Component, pageProps }) {
   return (
     <GoogleReCaptchaProvider
-      reCaptchaKey="6LdBzhUiAAAAABYm5986fFShQLFHVg7vQglOoPH5"
+      reCaptchaKey="6LdBzhUiAAAAAGnjMtWaqrFmFAG6gE_yM_LQq_tZ"
       scriptProps={{
         async: false,
         defer: false,
