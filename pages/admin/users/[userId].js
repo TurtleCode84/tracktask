f4f6bbaf619501876f913b894ef304019a8120a1
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-20 19:14:10 -0400

    Update [userId].js

--- pages/admin/users/[userId].js
+++ pages/admin/users/[userId].js
@@ -3,7 +3,7 @@ import Layout from "components/Layout";
 import Loading from "components/Loading";
 import useUser from "lib/useUser";
 import useAdminUser from "lib/useAdminUser";
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 import moment from "moment";
 import Link from "next/link";
 
@@ -24,17 +24,17 @@ export default function Admin() {
   return (
     <Layout>
       <h1>TrackTask User Admin &#128737;</h1>
-      <h2>
+      {/*<h2>
         Viewing information for {lookup.username}:
       </h2>
       <p>User ID: {lookup._id}</p>
-      {/*<p>Username: {lookup.user}</p>
+      <p>Username: {lookup.user}</p>
       <p>Email: <Link href={`mailto:${lookup.email}`}>{lookup.email}</Link></p>
       <p title={moment.unix(lookup.joined).format("dddd, MMMM Do YYYY, h:mm:ss a")}>Joined: {moment.unix(lookup.joined).fromNow()}</p>
       <p>Join IP address: <Link title="Lookup IP address" href={`https://whatismyipaddress.com/ip/${lookup.joinedIp}`}>{lookup.joinedIp}</Link></p>
       <p title={moment.unix(lookup.lastLogin).format("dddd, MMMM Do YYYY, h:mm:ss a")}>Last login: {moment.unix(lookup.lastLogin).fromNow()}</p>
       <p>Admin notes: {lookup.notes}</p>*/}
-      <pre>{JSON.stringify(getUser, null, 2)}</pre>
+      <pre>{JSON.stringify(lookup, null, 2)}</pre>
     </Layout>
   );
 }
