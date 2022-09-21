4ccdb2dc8fb6b93e8d150075208cba881c2e7c3d
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-21 09:38:18 -0400

    Update index.js

--- pages/admin/users/index.js
+++ pages/admin/users/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Layout from "components/Layout";
 import Loading from "components/Loading";
+import UserSearchForm from "components/UserSearchForm";
 import useUser from "lib/useUser";
+import Link from "next/link";
 
 export default function UsersAdmin() {
   const { user, mutateUser } = useUser({
@@ -20,9 +22,35 @@ export default function UsersAdmin() {
       <h2>
         Search for user:
       </h2>
-      <p style={{ fontStyle: "italic" }}>
-        Eventually, this page will contain a search bar to lookup a specific user.
-      </p>
+      <UserSearchForm
+          errorMessage={errorMsg}
+          onSubmit={async function handleSubmit(event) {
+            event.preventDefault();
+
+            const body = {
+              usernameuid: event.currentTarget.usernameuid.value,
+              query: event.currentTarget.query.value,
+            };
+
+            try {
+              mutateUser(
+                await fetchJson("/api/admin/users/search", {
+                  method: "POST",
+                  headers: { "Content-Type": "application/json" },
+                  body: JSON.stringify(body),
+                }),
+                false,
+              );
+            } catch (error) {
+              if (error instanceof FetchError) {
+                setErrorMsg(error.data.message);
+              } else {
+                console.error("An unexpected error happened:", error);
+              }
+            }
+          }}
+      />
+      <Link href="/admin/users">Back to search</Link>
     </Layout>
   );
 }
