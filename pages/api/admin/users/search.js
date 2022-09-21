e911b676ced6f49a55a42adf7feda2a55bed92be
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-21 10:11:40 -0400

    Create search.js

---
+++ pages/api/admin/users/search.js
@@ -0,0 +1,48 @@
+import { withIronSessionApiRoute } from "iron-session/next";
+import { sessionOptions } from "lib/session";
+import { ObjectId } from 'mongodb'
+import clientPromise from "lib/mongodb";
+
+export default withIronSessionApiRoute(adminUserSearchRoute, sessionOptions);
+
+async function adminUserSearchRoute(req, res) {
+  if (req.method === 'POST') {
+    const { usernameuid, query } = await req.body;
+    const user = req.session.user;
+    
+    if (!user || !user.isLoggedIn || !user.permissions.admin ) {
+      res.status(401).json({ message: "Unauthorized" });
+      return;
+    }
+    
+    const client = await clientPromise;
+    const db = client.db("data");
+    var dbQuery;
+    if (query === "username") {
+      dbQuery = { username: usernameuid) };
+    } else if (query === "uid") {
+      if (!ObjectId.isValid(usernameuid)) {
+        res.status(422).json({ message: "Invalid user ID" });
+        return;
+      }
+      dbQuery = { _id: ObjectId(uid) };
+    } else {
+      res.status(422).json({ message: "Invalid search query" });
+      return;
+    }
+    const options = { projection: { _id : 1 } };
+  
+    try {
+      const searchUser = await db.collection("users").findOne(dbQuery, options);
+      if (searchUser) {
+        res.json(searchUser);
+      } else {
+        res.status(422).json({ message: "User does not exist" });
+      }
+    } catch (error) {
+      res.status(200).json([]);
+    }
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+}
