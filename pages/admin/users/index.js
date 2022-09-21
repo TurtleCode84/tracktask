f30a30181bb29fb5c4b6679c50539761b8eeacc0
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-21 09:24:08 -0400

    Update index.js

--- pages/admin/users/index.js
+++ pages/admin/users/index.js
@@ -16,23 +16,13 @@ export default function UsersAdmin() {
   }
   return (
     <Layout>
-      <h1>TrackTask User Admin</h1>
+      <h1>TrackTask User Admin &#128737;</h1>
       <h2>
-        You shouldn&apos;t be here either...
+        Search for user:
       </h2>
       <p style={{ fontStyle: "italic" }}>
-        Luckily, there&apos;s not much here yet.
+        Eventually, this page will contain a search bar to lookup a specific user.
       </p>
-      <p>Eventually, this page will contain a search bar to lookup a specific user.</p>
-    {/*{user && (
-        <>
-          <p style={{ fontStyle: "italic" }}>
-            Your user info, pulled from the TrackTask API.
-          </p>
-
-          <pre>{JSON.stringify(user, null, 2)}</pre>
-        </>
-      )}*/}
     </Layout>
   );
 }
