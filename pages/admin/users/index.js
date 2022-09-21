7bad22d8f574913fbd179f051d78bcaa678d2fc1
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-21 09:52:21 -0400

    Update index.js

--- pages/admin/users/index.js
+++ pages/admin/users/index.js
@@ -8,12 +8,11 @@ import fetchJson, { FetchError } from "lib/fetchJson";
 import { useRouter } from 'next/router'
 
 export default function UsersAdmin() {
+  const router = useRouter();
   const { user, mutateUser } = useUser({
     redirectTo: "/dashboard",
     adminOnly: true,
   });
-  
-  const router = useRouter();
 
   if (!user || !user.isLoggedIn || !user.permissions.admin) {
     return (
