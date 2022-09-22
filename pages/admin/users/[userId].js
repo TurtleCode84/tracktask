36623b90bea31b9bc830aac6d0a31dff4e7c5a8b
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-22 07:42:39 -0400

    Update [userId].js

--- pages/admin/users/[userId].js
+++ pages/admin/users/[userId].js
@@ -40,7 +40,7 @@ export default function UserAdmin() {
       </h2>
       <Link href="/admin/users">Back to user search</Link><br/>
       {lookup ?
-      <>{lookup.permissions.banned && <p><b>This user is banned.</b></p>}{lookup.permissions.banned && lookup.history.banReason && <p style={{ fontStyle: "italic" }}>Reason: {
+      <>{lookup.permissions.banned && <p><b>This user is banned.</b></p>}{' '}{lookup.permissions.banned && lookup.history.banReason && <p style={{ fontStyle: "italic" }}>Reason: {lookup.history.banReason}</p>}
       <p>User ID: {lookup._id}</p>
       <p>Username: {lookup.username}</p>
       <p>Email: <a href={`mailto:${lookup.email}`} target="_blank" rel="noreferrer">{lookup.email}</a></p>
