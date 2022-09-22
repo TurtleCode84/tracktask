2897d4b9a1fea53450eba412efe6a00e10e8a0c5
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-22 13:10:12 -0400

    Update UserAdminForm.js

--- components/UserAdminForm.js
+++ components/UserAdminForm.js
@@ -67,7 +67,7 @@ export default function UserSearchForm({ errorMessage, onSubmit, lookup }) {
         input[type="checkbox"] {
           margin: 0;
           vertical-align: middle;
-          width: 5px !important;
+          width: 15px !important;
         }
         .error {
           color: brown;
