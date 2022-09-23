ae1b04e22453fe12925030a285f48ba6e69da513
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-23 15:10:33 -0400

    Update index.js

--- pages/api/admin/users/[uid]/index.js
+++ pages/api/admin/users/[uid]/index.js
@@ -36,7 +36,7 @@ async function adminUserRoute(req, res) {
     const body = await req.body;
     var updateUser = {};
     if (body.username) updateUser.username = body.username;
-    if (body.username) updateUser.email = body.email;
+    if (body.email) updateUser.email = body.email;
     if (body.password) updateUser.password = await hash(body.password, 10);
     if (body.shareKey) updateUser.shareKey = body.shareKey;
     if (body.profilePicture) updateUser.profilePicture = body.profilePicture;
