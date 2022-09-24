b1c2250f240743f9a233ab0f51365f1241f9c72c
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-23 20:57:55 -0400

    Update UserAdminForm.js

--- components/UserAdminForm.js
+++ components/UserAdminForm.js
@@ -24,7 +24,7 @@ export default function UserAdminForm({ errorMessage, onSubmit, lookup }) {
       </label>
       <label>
         <span>Admin notes</span>
-        <textarea name="notes" rows="4" cols="60" placeholder={lookup.history.notes} disabled />
+        <textarea name="notes" rows="4" cols="50" placeholder={lookup.history.notes} disabled />
       </label><hr/>
       <h2>Permissions:</h2>
       <label>
@@ -71,7 +71,7 @@ export default function UserAdminForm({ errorMessage, onSubmit, lookup }) {
           margin-bottom: 10px;
         }
         textarea {
-          resize: false;
+          resize: none;
         }
         .error {
           color: brown;
