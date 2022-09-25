4641ef29532ce230ad291dc01f9e2fe83a5e6a06
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-25 13:48:31 -0400

    Create index.js

---
+++ pages/api/admin/users/index.js
@@ -0,0 +1,46 @@
+import { withIronSessionApiRoute } from "iron-session/next";
+import { sessionOptions } from "lib/session";
+//import { ObjectId } from 'mongodb'
+import clientPromise from "lib/mongodb";
+
+export default withIronSessionApiRoute(adminUsersRoute, sessionOptions);
+
+async function adminUsersRoute(req, res) {
+  const user = req.session.user;
+  if (!user || !user.isLoggedIn || !user.permissions.admin ) {
+    res.status(401).json({ message: "Unauthorized" });
+    return;
+  }
+  if (req.method === 'GET') {
+    const { sort, count } = req.query;
+    if (!sort || !count) {
+      res.status(422).json({ message: "You must specify the query parameters \'sort\' and \'count\'" });
+      return;
+    }
+    if (sort === "joined") {
+      const client = await clientPromise;
+      const db = client.db("data");
+      const options = {
+        projection: { _id: 1, username: 1, 'history.joined': 1 },
+        sort: { 'history.joined': -1 },
+        limit: parseInt(count),
+      }
+  
+      try {
+        const getUsers = await db.collection("users").find(options);
+        if (getUsers) {
+          res.json(getUsers);
+        } else {
+          res.status(404).json({ message: "No users found" });
+        }
+      } catch (error) {
+        res.status(200).json([]); // ?
+      }
+    } else {
+      res.status(422).json({ message: "Invalid sort query" });
+      return;
+    }
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+}
