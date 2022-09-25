ccea39b53da02c0805d4cdcc383fb7242dac816d
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-25 17:35:43 -0400

    Create TaskCreateForm.js

---
+++ components/TaskCreateForm.js
@@ -0,0 +1,61 @@
+export default function TaskCreateForm({ errorMessage, onSubmit }) {
+  return (
+    <form id="userAdminForm" onSubmit={onSubmit}>
+      <label>
+        <span>Name</span>
+        <input type="text" name="name" required />
+      </label>
+      <label>
+        <span>Description</span>
+        <textarea name="description" rows="4" cols="30" required />
+      </label>
+      <label>
+        <span>Due Date (optional)</span>
+        <input type="datetime-local" name="dueDate" />
+      </label>
+      <label>
+        <span>Mark as priority</span>
+        <input type="checkbox" name="markPriority" />
+      </label>
+      <label>
+        <span>Mark as completed</span>
+        <input type="checkbox" name="markCompleted" />
+      </label>
+
+      <button type="submit" id="createTaskBtn">Create task</button>
+
+      {errorMessage && <p className="error">{errorMessage}</p>}
+
+      <style jsx>{`
+        form,
+        label {
+          display: flex;
+          flex-flow: column;
+        }
+        label > span {
+          font-weight: 600;
+        }
+        input {
+          padding: 8px;
+          margin: 0.3rem 0 1rem;
+          border: 1px solid #ccc;
+          border-radius: 4px;
+          max-width: 400px;
+        }
+        input[type="checkbox"] {
+          margin: 0;
+          vertical-align: middle;
+          width: 15px !important;
+          margin-bottom: 10px;
+        }
+        textarea {
+          resize: none;
+        }
+        .error {
+          color: brown;
+          margin: 1rem 0 0;
+        }
+      `}</style>
+    </form>
+  );
+}
