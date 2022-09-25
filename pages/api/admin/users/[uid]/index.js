ece99e5a6243297b42d3d7f9ac57e28ff1d39e77
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-24 21:45:06 -0400

    Update index.js

--- pages/api/admin/users/[uid]/index.js
+++ pages/api/admin/users/[uid]/index.js
@@ -35,7 +35,7 @@ async function adminUserRoute(req, res) {
   } else if (req.method === 'POST') {
     const body = await req.body;
     const { uid } = req.query;
-    if (process.env.SUPERADMIN === uid.toString() && user.id !== uid.toString()) {
+    if (process.env.SUPERADMIN === uid && user.id !== uid) {
       res.status(403).json({ message: "You do not have permission to modify this user." });
       return;
     }
