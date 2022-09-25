472ac1852058352da729bfb339c48d191c463480
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-25 17:49:56 -0400

    Create create.js

---
+++ pages/dashboard/tasks/create.js
@@ -0,0 +1,61 @@
+import React from "react";
+import moment from "moment";
+import Layout from "components/Layout";
+import Loading from "components/Loading";
+import TaskCreateForm from "components/TaskCreateForm";
+import useUser from "lib/useUser";
+import fetchJson, { FetchError } from "lib/fetchJson";
+
+export default function TasksCreate() {
+  const { user } = useUser({
+    redirectTo: "/login",
+  });
+  
+  const [errorMsg, setErrorMsg] = useState("");
+  const router = useRouter();
+  
+  if (!user || !user.isLoggedIn || user.permissions.banned) {
+    return (
+      <Loading/>
+    );
+  }
+  return (
+    <Layout>
+      <h1>
+        Create a task:
+      </h1>
+      <h2>General information</h2>
+      <TaskCreateForm
+        errorMessage={errorMsg}
+        onSubmit={async function handleSubmit(event) {
+          event.preventDefault();
+          document.getElementById("createTaskBtn").disabled = true;
+
+          const body = {
+            name: event.currentTarget.name.value,
+            description: event.currentTarget.description.value,
+            dueDate: event.currentTarget.dueDate.value,
+            markCompleted: event.currentTarget.markCompleted.checked,
+            markPriority: event.currentTarget.markPriority.checked,
+          };
+
+          try {
+            const getUrl = await fetchJson("/api/tasks", {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify(body),
+            })
+            router.push(`/dashboard`);
+          } catch (error) {
+            if (error instanceof FetchError) {
+              setErrorMsg(error.data.message);
+            } else {
+              console.error("An unexpected error happened:", error);
+            }
+            document.getElementById("createTaskBtn").disabled = false;
+          }
+        }}
+      />
+    </Layout>
+  );
+}
