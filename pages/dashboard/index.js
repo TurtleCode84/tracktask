f20be09e16a70a2fa0ec612a04f0d2efe25b6919
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-26 13:22:46 -0400

    Update index.js

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -12,9 +12,7 @@ consolere.connect({
   channel: 'tracktask', // required
   redirectDefaultConsoleToRemote: true, // optional, default: false
 });
-consolere.ready(function () {
-  console.re.log('remote log test');
-});
+console.re.log('remote log test');
 
 // Make sure to check https://nextjs.org/docs/basic-features/layouts for more info on how to use layouts
 export default function Dashboard() {
