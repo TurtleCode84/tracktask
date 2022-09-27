ff964d17116670ce0d86d98adcb89fe65eae75de
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-27 08:46:16 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,18 +4,4 @@ module.exports = {
   images: {
     domains: ["avatars.githubusercontent.com", "u.cubeupload.com", "i.bb.co", "api.wasteof.money"],
   },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  },
 };
