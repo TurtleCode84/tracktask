72328a43e0dc5fe722b03d50dfa67e06c72ae90d
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-27 09:03:11 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,28 +1,4 @@
 module.exports = {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://dev-tracktask.vercel.app" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,DELETE,POST" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      },
-      {
-        // matching /dashboard
-        source: "/dashboard",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://wasteof.money" },
-          { key: "Access-Control-Allow-Methods", value: "GET" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      },
-    ]
-  },
   reactStrictMode: true,
   images: {
     domains: ["avatars.githubusercontent.com", "u.cubeupload.com", "i.bb.co", "api.wasteof.money"],
