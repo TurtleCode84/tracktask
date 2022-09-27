6932dd11abbc5dfec3ba8b16b68d27e280608565
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-27 13:55:13 -0400

    Update tasks.js

--- pages/api/tasks.js
+++ pages/api/tasks.js
@@ -45,7 +45,7 @@ async function tasksRoute(req, res) {
         collections: [],
       }
       if (dueDate) {
-        newTask.dueDate = moment(dueDate, moment.HTML5_FMT.DATETIME_LOCAL).utc().unix();
+        newTask.dueDate = moment(dueDate, moment.HTML5_FMT.DATETIME_LOCAL).local().unix();
       } else {
         newTask.dueDate = 0;
       }
