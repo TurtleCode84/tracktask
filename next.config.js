d82d4488f41946c09ae9cfcede64a8a1328e4e12
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-27 08:34:54 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,4 +4,18 @@ module.exports = {
   images: {
     domains: ["avatars.githubusercontent.com", "u.cubeupload.com", "i.bb.co", "api.wasteof.money"],
   },
+  async headers() {
+    return [
+      {
+        // matching admin API routes
+        source: "/api/admin/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://tracktask.vercel.app" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
 };
