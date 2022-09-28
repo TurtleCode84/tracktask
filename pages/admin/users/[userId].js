6600be061fe16dd2454667afd2b52d453ad0700b
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-28 13:12:19 -0400

    Update [userId].js

--- pages/admin/users/[userId].js
+++ pages/admin/users/[userId].js
@@ -62,14 +62,14 @@ export default function UserAdmin() {
       <details>
         <summary>Last 5 IP addresses</summary>
         <p style={{ fontStyle: "italic" }}>(Newest to oldest)</p>
-        <ul>{ipList?.length > 0 ? ipList : 'No IPs found'}</ul>
+        <ul>{ipList && ipList.length > 0 ? ipList : 'No IPs found'}</ul>
       </details>
       <p title={moment.unix(lookup.history.lastLogin).format("dddd, MMMM Do YYYY, h:mm:ss a")}>Last login: {lookup.history.lastLogin > 0 ? moment.unix(lookup.history.lastLogin).fromNow() : 'never'}</p>
       <p>Admin notes: {lookup.history.notes ? lookup.history.notes : 'none'}</p>
       <details>
         <summary>Warnings</summary>
         <p style={{ fontStyle: "italic" }}>(Newest to oldest)</p>
-        <ul>{warningList?.length > 0 ? warningList : 'No warnings found'}</ul>
+        <ul>{warningList && warningList.length > 0 ? warningList : 'No warnings found'}</ul>
       </details>
       {!lookup.permissions.banned && <p>Last ban reason: {lookup.history.banReason ? lookup.history.banReason : 'none'}</p>}
       <hr/>
