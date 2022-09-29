e4b70ae32606626e0228739dfe1ad9a8e757cbe9
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-28 20:56:55 -0400

    Update index.js

--- pages/api/admin/users/[uid]/index.js
+++ pages/api/admin/users/[uid]/index.js
@@ -93,8 +93,9 @@ async function adminUserRoute(req, res) {
       res.status(403).json({ message: "You do not have permission to delete users." });
       return;
     }
-    const query = { _id: uid };
-    const deletedUser = await db.collection('users').deleteOne(query);
+    const deletedUser = await db.collection('users').deleteOne({ _id: ObjectId(uid) });
+    const deletedTasks = await db.collection('tasks').deleteOne({ owner: ObjectId(uid) }); // See above
+    //const deletedCollections = await db.collection('collections').deleteOne({ owner: ObjectId(uid) }); // See above
     res.json(deletedUser);
   } else {
     res.status(405).json({ message: "Method not allowed" });
