a32734091b73161874ef803b4335383851bfe48a
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-28 22:08:11 -0400

    Update user.js

--- pages/api/user.js
+++ pages/api/user.js
@@ -6,30 +6,37 @@ import clientPromise from "lib/mongodb";
 export default withIronSessionApiRoute(userRoute, sessionOptions);
 
 async function userRoute(req, res) {
-  if (req.session.user) {
-    const client = await clientPromise;
-    const db = client.db("data");
-    const query = { _id: ObjectId(req.session.user.id) };
-    //const options = { projection: { permissions: 1 } };
-    const userInfo = await db.collection("users").findOne(query);
-    const user = {
-      ...req.session.user,
-      isLoggedIn: true,
-      email: userInfo.email,
-      profilePicture: userInfo.profilePicture,
-      history: { joined: userInfo.history.joined, banReason: userInfo.history.banReason },
-      shareKey: userInfo.shareKey,
-      permissions: userInfo.permissions,
-    };
-    req.session.user = user;
-    await req.session.save();
-    res.json(user);
+  if (req.method === 'POST') {
+    if (req.session.user) {
+      const client = await clientPromise;
+      const db = client.db("data");
+      const query = { _id: ObjectId(req.session.user.id) };
+      const userInfo = await db.collection("users").findOne(query);
+      const user = {
+        ...req.session.user,
+        isLoggedIn: true,
+        email: userInfo.email,
+        profilePicture: userInfo.profilePicture,
+        history: { joined: userInfo.history.joined, banReason: userInfo.history.banReason },
+        shareKey: userInfo.shareKey,
+        permissions: userInfo.permissions,
+      };
+      req.session.user = user;
+      await req.session.save();
+      res.json(user);
+    } else {
+      res.json({
+        isLoggedIn: false,
+        id: "",
+        username: "",
+        permissions: {},
+      });
+    }
+  } else if (req.method === 'POST') {
+    res.status(418).json({ message: "Under construction" });
+    return;
   } else {
-    res.json({
-      isLoggedIn: false,
-      id: "",
-      username: "",
-      permissions: {},
-    });
+    res.status(405).json({ message: "Method not allowed" });
+    return;
   }
 }
