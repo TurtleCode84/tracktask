a5a03c5a46b3c6a579448a68a8a91c8bc4a0e185
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-29 20:25:34 -0400

    Fix CSS

--- components/Layout.js
+++ components/Layout.js
@@ -32,8 +32,7 @@ export default function Layout({ children }) {
         .list {
           margin: "0.5em";
           padding: "5px 5px 5px 5px";
-          border-style: "ridge";
-          border-weight: "5px";
+          border: "5px ridge rgb(0, 0, 0)";
           border-radius: "10px";
           width: "auto";
           transition: background 1s;
