e7d9dc2fe4ff443383128f7ee54a53c243d4d43a
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-09-30 20:18:34 -0400

    Update and rename [tid].js to [id].js

---
+++ pages/api/admin/tasks/[id].js
@@ -0,0 +1,51 @@
+import { withIronSessionApiRoute } from "iron-session/next";
+import { sessionOptions } from "lib/session";
+import { ObjectId } from 'mongodb'
+import clientPromise from "lib/mongodb";
+
+export default withIronSessionApiRoute(adminTaskRoute, sessionOptions);
+
+async function adminUserRoute(req, res) {
+  if (req.method === 'GET') {
+    const user = req.session.user;
+    if (!user || !user.isLoggedIn || !user.permissions.admin ) {
+      res.status(401).json({ message: "Unauthorized" });
+      return;
+    }
+    
+    const { id, collection } = req.query
+    if (!ObjectId.isValid(id)) {
+      res.status(422).json({ message: "Invalid object ID" });
+      return;
+    }
+    const client = await clientPromise;
+    const db = client.db("data");
+    const query = {
+      _id: ObjectId(id),
+      $or: [
+        { owner: user.id },
+        { 'sharing.sharedWith': {$elemMatch: {id: user.id}} },
+      ],
+    };
+    try {
+      var getTasks;
+      if (!collection) {
+        getTasks = await db.collection("tasks").findOne(query);
+      } else {
+        getTasks = await db.collection("collections").findOne(query);
+      }
+      res.json(getTasks);
+    } catch (error) {
+      res.status(200).json([]);
+    }
+  } else if (req.method === 'POST') {
+    res.status(418).json({ message: "Under construction" });
+    return;
+  } else if (req.method === 'DELETE') {
+    res.status(418).json({ message: "Under construction" });
+    return;
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+    return;
+  }
+}
