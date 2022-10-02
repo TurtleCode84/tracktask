11bb69a7cc67b023dbc66d10449f637c8f019f2f
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-02 11:27:59 -0400

    Big update: collections

--- pages/api/tasks/index.js
+++ pages/api/tasks/index.js
@@ -14,16 +14,42 @@ async function tasksRoute(req, res) {
   }
   const client = await clientPromise;
   const db = client.db("data");
-  if (req.method === 'GET') { // Get all unhidden tasks for the logged in user
-    const query = { owner: ObjectId(user.id), hidden: false };
-    const options = {
-      sort: { priority: -1, dueDate: 1 },
-      projection: { name: 1, description: 1, dueDate: 1, owner: 1, completion: 1, priority: 1, collections: 1 },
+  if (req.method === 'GET') { // Get all unhidden tasks or collections for the logged in user
+    const { collections } = req.query;
+    //const query = { owner: ObjectId(user.id), hidden: false };
+    const query = {
+      hidden: false,
+      $or: [
+        { owner: ObjectId(user.id) },
+        { 'sharing.shared': true, 'sharing.sharedWith': {$elemMatch: {id: ObjectId(user.id)}} },
+      ],
     };
-
+    const taskoptions = {
+      sort: { priority: -1, dueDate: 1 },
+      projection: { name: 1, description: 1, dueDate: 1, owner: 1, completion: 1, priority: 1 },
+    };
+    var data;
     try {
-      const tasks = await db.collection("tasks").find(query, options).toArray();
-      res.json(tasks);
+      if (collections !== "true") {
+        data = await db.collection("tasks").find(query, taskoptions).toArray();
+      } else {
+        const collectionoptions = {
+          sort: { created: -1 },
+          projection: { name: 1, description: 1, created: 1, owner: 1, sharing: 1, tasks: 1 },
+        }
+        data = await db.collection("collections").find(query, collectionoptions).toArray();
+        for (var i; i<data.length; i++) {
+          data[i].tasks = await db.collection("tasks").find({ _id: {$in: data[i].tasks} }, taskoptions).toArray();
+        }
+      }
+      if (!data && collections !== "true") {
+        res.status(404).json({ message: "No tasks found" });
+        return;
+      } else if (!data) {
+        res.status(404).json({ message: "No collection found" });
+        return;
+      }
+      res.json(data);
     } catch (error) {
       res.status(200).json([]);
     }
