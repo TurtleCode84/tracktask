ccd47c636b3a716d9c73fd5e975201245e9afd4e
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-02 18:29:23 -0400

    Implement collections

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -12,12 +12,18 @@ export default function Dashboard() {
     redirectTo: "/login",
   });
   
-  const { tasks } = useTasks(user);
+  const { tasks } = useTasks(user, false, "upcoming");
   const router = useRouter();
-  const sortedTasks = tasks?.sort((a, b) => (a.dueDate === 0 || b.priority) ? 1 : -1);
+  const sortedTasks = tasks?.sort((a, b) => (a.priority) ? 1 : -1);
   const taskList = sortedTasks?.map((task) =>
     <li key={task._id} className="list-hover" style={{ margin: "0.5em", background: "#f8f8f8", padding: "5px", borderWidth: "2px", borderStyle: "solid", borderColor: "darkgray", borderRadius: "10px", width: "auto" }} onClick={() => router.push(`/tasks/${task._id}`)}>
       {task.priority ? <>&#10071;</> : null}<b>{task.name}</b> - {task.description.slice(0,25).trim()}... (due <DueDate timestamp={task.dueDate}/>{task.dueDate !== 0 ? <>, on {moment.unix(task.dueDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}</> : null})
+    </li>
+  );
+  const { collections } = useTasks(user, true, false);
+  const collectionList = collections?.map((collection) =>
+    <li key={collection._id} className="list-hover" style={{ margin: "0.5em", background: "#f8f8f8", padding: "5px", borderWidth: "2px", borderStyle: "solid", borderColor: "darkgray", borderRadius: "10px", width: "auto" }} onClick={() => router.push(`/collections/${collection._id}`)}>
+      {collection.owner === user.id ? <>&#128273;</> : null}<b>{collection.name}</b> - {collection.description.slice(0,25).trim()}... (created <DueDate timestamp={collection.created}/>)
     </li>
   );
   
@@ -40,10 +46,10 @@ export default function Dashboard() {
         }
       </h1>
 
-      <h2>Your tasks:</h2>
+      <h2>Upcoming tasks:</h2>
       {taskList === undefined || taskList.length === 0 ?
       <>
-      {taskList?.length === 0 ? <p style={{ fontStyle: "italic" }}>Looks like you have no tasks!</p> : <p style={{ fontStyle: "italic" }}>Loading tasks...</p>}
+      {taskList?.length === 0 ? <p style={{ fontStyle: "italic" }}>You have no upcoming tasks, great job!</p> : <p style={{ fontStyle: "italic" }}>Loading tasks...</p>}
       </>
       :
       <><ul style={{ display: "table" }}>
@@ -52,9 +58,15 @@ export default function Dashboard() {
       }
       
       <h2>Your collections:</h2>
-      <p style={{ fontStyle: "italic" }}>
-        Coming soon...
-      </p>
+      {collectionList === undefined || collectionList.length === 0 ?
+      <>
+      {collectionList?.length === 0 ? <p style={{ fontStyle: "italic" }}>You haven&apos;t created or joined any collections.</p> : <p style={{ fontStyle: "italic" }}>Loading collections...</p>}
+      </>
+      :
+      <><ul style={{ display: "table" }}>
+        {collectionList}
+      </ul></>
+      }
     </Layout>    
   );
 }
