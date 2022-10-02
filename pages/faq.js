6958abf859ebdc2fdb94a526c89b36f25a4fdc60
Ankit_Anmol <87054411+Quantum-Codes@users.noreply.github.com>
2022-10-02 17:06:08 +0530

    Commas?

--- pages/faq.js
+++ pages/faq.js
@@ -22,11 +22,11 @@ export default function Faq() {
       </p>
 
       <ul>
-        <li><p style={{ fontStyle: "italic"; font-weight: "bold"; }}>Can you add...?</p><p>Feature requests likely won&apos;t be considered until TrackTask is officially announced as ready for the public (meaning all of critical features are in place).</p></li>
-        <li><p style={{ fontStyle: "italic"; font-weight: "bold"; }}>This is cool, how can I help out?</p><p>Our public GitHub repository with contributor instructions is in the making, check back soon!</p></li>
-        <li><p style={{ fontStyle: "italic"; font-weight: "bold"; }}>What does it mean to be verified?</p><p>If your account is verified, you will be able to share collections of tasks with other users, among other perks. This is to prevent abuse of the sharing capabilities by restricting them to reviewed & trusted users.</p></li>
-        <li><p style={{ fontStyle: "italic"; font-weight: "bold"; }}>How do I get verified?</p><p>There is currently no official way to request verification, so at the moment it&apos;s given only at admin discretion.</p></li>
-        <li><p style={{ fontStyle: "italic"; font-weight: "bold"; }}>Help, I&apos;ve been banned!</p><p>Unfortunately for you, there is no official way to appeal to a ban yet, so you&apos;ll either have find an admin to contact or wait until the next update...</p></li>
+        <li><p style={{ fontStyle: "italic", font-weight: "bold"; }}>Can you add...?</p><p>Feature requests likely won&apos;t be considered until TrackTask is officially announced as ready for the public (meaning all of critical features are in place).</p></li>
+        <li><p style={{ fontStyle: "italic", font-weight: "bold"; }}>This is cool, how can I help out?</p><p>Our public GitHub repository with contributor instructions is in the making, check back soon!</p></li>
+        <li><p style={{ fontStyle: "italic", font-weight: "bold"; }}>What does it mean to be verified?</p><p>If your account is verified, you will be able to share collections of tasks with other users, among other perks. This is to prevent abuse of the sharing capabilities by restricting them to reviewed & trusted users.</p></li>
+        <li><p style={{ fontStyle: "italic", font-weight: "bold"; }}>How do I get verified?</p><p>There is currently no official way to request verification, so at the moment it&apos;s given only at admin discretion.</p></li>
+        <li><p style={{ fontStyle: "italic", font-weight: "bold"; }}>Help, I&apos;ve been banned!</p><p>Unfortunately for you, there is no official way to appeal to a ban yet, so you&apos;ll either have find an admin to contact or wait until the next update...</p></li>
       </ul>
 
       <style jsx>{`
