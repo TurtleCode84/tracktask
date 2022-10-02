24243d98f00a04777a3a63a2acc8e2224f947cf7
Ankit_Anmol <87054411+Quantum-Codes@users.noreply.github.com>
2022-10-02 15:36:07 +0530

    Try adding limits to API

--- pages/api/tasks/index.js
+++ pages/api/tasks/index.js
@@ -33,6 +33,10 @@ async function tasksRoute(req, res) {
       res.status(422).json({ message: "Invalid data" });
       return;
     }
+    if (name.length > 50 || description.length > 500) {
+      res.status(422).json({ message: "Invalid data. Length of title and description must not exceed 50 and 500 characters respectively." });
+      return;
+    }
     // Otherwise...
     try {
       const newTask = {
