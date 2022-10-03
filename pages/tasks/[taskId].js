363afd56b5ce037442b4768bace2f2436b3c6a41
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-03 08:58:54 -0400

    Cleanup, remove unnecessary optionals

--- pages/tasks/[taskId].js
+++ pages/tasks/[taskId].js
@@ -28,17 +28,17 @@ export default function Task() {
   
   return (
     <Layout>
-      <h2>{task ? task.name : 'Loading...'}:</h2>
+      <h2>{task ? <>{task.name}:</> : 'Loading...'}</h2>
       <Link href="/dashboard">Back to dashboard</Link><br/>
       {task ?
         <><h3>General information</h3>
-        <p>Description: {task?.description}</p>
-        <p title={moment.unix(task?.dueDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}>Due date: {task?.dueDate > 0 ? <>{moment.unix(task?.dueDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}{' '}({moment.unix(task?.dueDate).fromNow()})</> : 'never'}</p>
-        <p>Priority: {task?.priority ? <>&#9989;</> : <>&#10060;</>}</p>
-        <p>Completed: {task?.completion.completed > 0 ? <>&#9989;</> : <>&#10060;</>}</p>
-        {task?.completion.completed > 0 && <>
-        <p>Completed on: {moment.unix(task?.completion.completed).format("dddd, MMMM Do YYYY, h:mm:ss a")}{' '}({moment.unix(task?.completion.completed).fromNow()})</p>
-        <p>Completed by: {task?.completion.completedBy}</p>
+        <p>Description: {task.description}</p>
+        <p title={moment.unix(task.dueDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}>Due date: {task.dueDate > 0 ? <>{moment.unix(task.dueDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}{' '}({moment.unix(task.dueDate).fromNow()})</> : 'never'}</p>
+        <p>Priority: {task.priority ? <>&#9989;</> : <>&#10060;</>}</p>
+        <p>Completed: {task.completion.completed > 0 ? <>&#9989;</> : <>&#10060;</>}</p>
+        {task.completion.completed > 0 && <>
+        <p>Completed on: {moment.unix(task.completion.completed).format("dddd, MMMM Do YYYY, h:mm:ss a")}{' '}({moment.unix(task.completion.completed).fromNow()})</p>
+        <p>Completed by: {task.completion.completedBy}</p>
         </>}
         <hr/>
         <details>
