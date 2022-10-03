080b033425fb005b00ffe36f5ae3aee65f63b6c8
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-03 08:37:56 -0400

    Ok, then why won't you work?

--- pages/tasks/[taskId].js
+++ pages/tasks/[taskId].js
@@ -30,24 +30,24 @@ export default function Task() {
     <Layout>
       <h2>{task ? task.name : 'Loading...'}:</h2>
       <Link href="/dashboard">Back to dashboard</Link><br/>
-    {/*{task ?
-      <>*/}<h3>General information</h3>
-      <p>Description: {task?.description}</p>
-      <p title={moment.unix(task?.dueDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}>Due date: {task?.dueDate > 0 ? <>{moment.unix(task?.dueDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}{' '}({moment.unix(task?.dueDate).fromNow()})</> : 'never'}</p>
-      <p>Priority: {task?.priority ? <>&#9989;</> : <>&#10060;</>}</p>
-      <p>Completed: {task?.completion.completed > 0 ? <>&#9989;</> : <>&#10060;</>}</p>
-      {task?.completion.completed > 0 && <>
-      <p>Completed on: {moment.unix(task?.completion.completed).format("dddd, MMMM Do YYYY, h:mm:ss a")}{' '}({moment.unix(task?.completion.completed).fromNow()})</p>
-      <p>Completed by: {task?.completion.completedBy}</p>
-      </>}
-      <hr/>
-      <details>
-        <summary>Edit task</summary>
-        <p style={{ fontStyle: "italic" }}>(Coming soon...)</p>
-      </details>{/*</>
+      {task ?
+        <><h3>General information</h3>
+        <p>Description: {task?.description}</p>
+        <p title={moment.unix(task?.dueDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}>Due date: {task?.dueDate > 0 ? <>{moment.unix(task?.dueDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}{' '}({moment.unix(task?.dueDate).fromNow()})</> : 'never'}</p>
+        <p>Priority: {task?.priority ? <>&#9989;</> : <>&#10060;</>}</p>
+        <p>Completed: {task?.completion.completed > 0 ? <>&#9989;</> : <>&#10060;</>}</p>
+        {task?.completion.completed > 0 && <>
+        <p>Completed on: {moment.unix(task?.completion.completed).format("dddd, MMMM Do YYYY, h:mm:ss a")}{' '}({moment.unix(task?.completion.completed).fromNow()})</p>
+        <p>Completed by: {task?.completion.completedBy}</p>
+        </>}
+        <hr/>
+        <details>
+          <summary>Edit task</summary>
+          <p style={{ fontStyle: "italic" }}>(Coming soon...)</p>
+        </details></>
       :
-      <>{error ? <p>{error.data.message}</p> : <p style={{ fontStyle: "italic" }}>Loading task...</p>}</>
-      }*/}
+        <>{error ? <p>{error.data.message}</p> : <p style={{ fontStyle: "italic" }}>Loading task...</p>}</>
+      }
     </Layout>
   );
 }
