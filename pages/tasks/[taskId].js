7c33c402c7212f742d85080a2502d3ea896b0c01
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-02 20:45:32 -0400

    Update [taskId].js

--- pages/tasks/[taskId].js
+++ pages/tasks/[taskId].js
@@ -18,7 +18,7 @@ export default function Task() {
   //const [errorMsg, setErrorMsg] = useState("");
   const router = useRouter();
   const { taskId } = router.query;
-  const preTask = tasks?.filter(item => item._id === taskId);
+  const preTask = await tasks?.filter(item => item._id === taskId);
   const task = preTask[0];
   
   if (!user || !user.isLoggedIn || user.permissions.banned) {
