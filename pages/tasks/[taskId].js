8b6021a532c05b9302171231b0dbf19fa37186cc
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-02 21:23:07 -0400

    Welp, fine then

--- pages/tasks/[taskId].js
+++ pages/tasks/[taskId].js
@@ -30,10 +30,10 @@ export default function Task() {
     <Layout>
       <h2>{task ? task.name : 'Loading...'}:</h2>
       <Link href="/dashboard">Back to dashboard</Link><br/>
-      {task ? <>
-      <h3>General information</h3>
-      <p>Description: {task?.description}</p>
-      {/*<p title={moment.unix(task.dueDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}>Due date: {task.dueDate > 0 ? <>{moment.unix(task.dueDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}{' '}({moment.unix(task.dueDate).fromNow()})</> : 'never'}</p>
+      {task ?
+      <><h3>General information</h3>
+      <p>Description: {task.description}</p>
+      <p title={moment.unix(task.dueDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}>Due date: {task.dueDate > 0 ? <>{moment.unix(task.dueDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}{' '}({moment.unix(task.dueDate).fromNow()})</> : 'never'}</p>
       <p>Priority: {task.priority ? <>&#9989;</> : <>&#10060;</>}</p>
       <p>Completed: {task.completion.completed > 0 ? <>&#9989;</> : <>&#10060;</>}</p>
       {task.completion.completed > 0 && <>
@@ -44,7 +44,7 @@ export default function Task() {
       <details>
         <summary>Edit task</summary>
         <p style={{ fontStyle: "italic" }}>(Coming soon...)</p>
-      </details>*/}</>
+      </details></>
       :
       <>{error ? <p>{error.data.message}</p> : <p style={{ fontStyle: "italic" }}>Loading task...</p>}</>
       }
