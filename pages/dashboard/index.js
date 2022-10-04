c195d52459652875b3efb0088b8357ee6cf95e1f
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-04 18:06:23 -0400

    Fix task sort, add recent tasks

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -12,10 +12,16 @@ export default function Dashboard() {
     redirectTo: "/login",
   });
   
-  const { tasks } = useTasks(user, false, "upcoming");
+  const { tasks: upcomingTasks } = useTasks(user, false, "upcoming");
   const router = useRouter();
-  const sortedTasks = tasks?.sort((a, b) => (a.priority) ? 1 : -1);
-  const taskList = sortedTasks?.map((task) =>
+  const sortedTasks = upcomingTasks?.sort((a, b) => (b.priority) ? 1 : -1);
+  const upcomingTaskList = sortedTasks?.map((task) =>
+    <li key={task._id} className="list-hover" style={{ margin: "0.5em", background: "#f8f8f8", padding: "5px", borderWidth: "2px", borderStyle: "solid", borderColor: "darkgray", borderRadius: "10px", width: "auto" }} onClick={() => router.push(`/tasks/${task._id}`)}>
+      {task.priority ? <><span title="Priority">&#10071;</span></> : null}<b>{task.name}</b> - {task.description.slice(0,25).trim()}... (due <DueDate timestamp={task.dueDate}/>{task.dueDate !== 0 ? <>, on {moment.unix(task.dueDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}</> : null})
+    </li>
+  );
+  const { tasks: recentTasks } = useTasks(user, false, "recent");
+  const recentTaskList = recentTasks?.map((task) =>
     <li key={task._id} className="list-hover" style={{ margin: "0.5em", background: "#f8f8f8", padding: "5px", borderWidth: "2px", borderStyle: "solid", borderColor: "darkgray", borderRadius: "10px", width: "auto" }} onClick={() => router.push(`/tasks/${task._id}`)}>
       {task.priority ? <><span title="Priority">&#10071;</span></> : null}<b>{task.name}</b> - {task.description.slice(0,25).trim()}... (due <DueDate timestamp={task.dueDate}/>{task.dueDate !== 0 ? <>, on {moment.unix(task.dueDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}</> : null})
     </li>
@@ -47,13 +53,24 @@ export default function Dashboard() {
       </h1>
 
       <h2>Upcoming tasks:</h2>
-      {taskList === undefined || taskList.length === 0 ?
+      {upcomingTaskList === undefined || upcomingTaskList.length === 0 ?
       <>
-      {taskList?.length === 0 ? <p style={{ fontStyle: "italic" }}>You have no upcoming tasks, great job!</p> : <p style={{ fontStyle: "italic" }}>Loading tasks...</p>}
+      {upcomingTaskList?.length === 0 ? <p style={{ fontStyle: "italic" }}>You have no upcoming tasks, great job!</p> : <p style={{ fontStyle: "italic" }}>Loading tasks...</p>}
       </>
       :
       <><ul style={{ display: "table" }}>
-        {taskList}
+        {upcomingTaskList}
+      </ul></>
+      }
+      
+      <h2>Recently added:</h2>
+      {recentTaskList === undefined || recentTaskList.length === 0 ?
+      <>
+      {recentTaskList?.length === 0 ? <p style={{ fontStyle: "italic" }}>You have no recent tasks.</p> : <p style={{ fontStyle: "italic" }}>Loading tasks...</p>}
+      </>
+      :
+      <><ul style={{ display: "table" }}>
+        {recentTaskList}
       </ul></>
       }
       
