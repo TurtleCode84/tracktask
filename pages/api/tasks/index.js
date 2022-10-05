ec9bf85cc81db36ee85b856f99f05c335040c687
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-04 21:12:58 -0400

    Test

--- pages/api/tasks/index.js
+++ pages/api/tasks/index.js
@@ -24,7 +24,7 @@ async function tasksRoute(req, res) {
       ],
     };
     const taskoptions = {
-      sort: { dueDate: 1, priority: -1 },
+      sort: { priority: -1, dueDate: 1 },
       projection: { name: 1, description: 1, dueDate: 1, owner: 1, completion: 1, priority: 1 },
     };
     var data;
