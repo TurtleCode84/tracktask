f06bb77ab8f79ebd7b438c75d3af2d02bfcbf5ed
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-05 14:30:05 -0400

    Catch and display task/collection errors

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -12,20 +12,20 @@ export default function Dashboard() {
     redirectTo: "/login",
   });
   
-  const { tasks: upcomingTasks } = useTasks(user, false, "upcoming");
+  const { tasks: upcomingTasks, error: upcomingTasksError } = useTasks(user, false, "upcoming");
   const router = useRouter();
   const upcomingTaskList = upcomingTasks?.map((task) =>
     <li key={task._id} className="list-hover" style={{ margin: "0.5em", background: "#f8f8f8", padding: "5px", borderWidth: "2px", borderStyle: "solid", borderColor: "darkgray", borderRadius: "10px", width: "auto" }} onClick={() => router.push(`/tasks/${task._id}`)}>
       {task.completion.completed !== 0 ? <><span title="Completed">&#9989;</span></> : null}{task.priority ? <><span title="Priority">&#10071;</span></> : null}<b>{task.name}</b> - {task.description.slice(0,25).trim()}... (due <DueDate timestamp={task.dueDate}/>{task.dueDate !== 0 ? <>, on {moment.unix(task.dueDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}</> : null})
     </li>
   );
-  const { tasks: recentTasks } = useTasks(user, false, "recent");
+  const { tasks: recentTasks, error: recentTasksError } = useTasks(user, false, "recent");
   const recentTaskList = recentTasks?.map((task) =>
     <li key={task._id} className="list-hover" style={{ margin: "0.5em", background: "#f8f8f8", padding: "5px", borderWidth: "2px", borderStyle: "solid", borderColor: "darkgray", borderRadius: "10px", width: "auto" }} onClick={() => router.push(`/tasks/${task._id}`)}>
       {task.completion.completed !== 0 ? <><span title="Completed">&#9989;</span></> : null}{task.priority ? <><span title="Priority">&#10071;</span></> : null}<b>{task.name}</b> - {task.description.slice(0,25).trim()}... (due <DueDate timestamp={task.dueDate}/>{task.dueDate !== 0 ? <>, on {moment.unix(task.dueDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}</> : null})
     </li>
   );
-  const { tasks: collections } = useTasks(user, true, false);
+  const { tasks: collections, error: collectionsError } = useTasks(user, true, false);
   const collectionList = collections?.map((collection) =>
     <li key={collection._id} className="list-hover" style={{ margin: "0.5em", background: "#f8f8f8", padding: "5px", borderWidth: "2px", borderStyle: "solid", borderColor: "darkgray", borderRadius: "10px", width: "auto" }} onClick={() => router.push(`/collections/${collection._id}`)}>
       {collection.owner === user.id ? <><span title="You own this collection">&#128273;</span>{' '}</> : null}<b>{collection.name}</b> - {collection.description.slice(0,25).trim()}... (created <DueDate timestamp={collection.created}/>)
@@ -52,9 +52,9 @@ export default function Dashboard() {
       </h1>
 
       <h2>Upcoming tasks:</h2>
-      {upcomingTaskList === undefined || upcomingTaskList.length === 0 ?
+      {upcomingTaskList === undefined || upcomingTasksError ?
       <>
-      {upcomingTaskList?.length === 0 ? <p style={{ fontStyle: "italic" }}>You have no upcoming tasks, great job!</p> : <p style={{ fontStyle: "italic" }}>Loading tasks...</p>}
+      {upcomingTasksError ? <p style={{ fontStyle: "italic" }}>{upcomingTasksError}</p> : <p style={{ fontStyle: "italic" }}>Loading tasks...</p>}
       </>
       :
       <><ul style={{ display: "table" }}>
@@ -63,9 +63,9 @@ export default function Dashboard() {
       }
       
       <h2>Recently added:</h2>
-      {recentTaskList === undefined || recentTaskList.length === 0 ?
+      {recentTaskList === undefined || recentTasksError ?
       <>
-      {recentTaskList?.length === 0 ? <p style={{ fontStyle: "italic" }}>You have no recent tasks.</p> : <p style={{ fontStyle: "italic" }}>Loading tasks...</p>}
+      {recentTasksError ? <p style={{ fontStyle: "italic" }}>{recentTasksError}</p> : <p style={{ fontStyle: "italic" }}>Loading tasks...</p>}
       </>
       :
       <><ul style={{ display: "table" }}>
@@ -74,9 +74,9 @@ export default function Dashboard() {
       }
       
       <h2>Your collections:</h2>
-      {collectionList === undefined || collectionList.length === 0 ?
+      {collectionList === undefined || collectionsError ?
       <>
-      {collectionList?.length === 0 ? <p style={{ fontStyle: "italic" }}>You haven&apos;t created or joined any collections.</p> : <p style={{ fontStyle: "italic" }}>Loading collections...</p>}
+      {collectionsError ? <p style={{ fontStyle: "italic" }}>{collectionsError}</p> : <p style={{ fontStyle: "italic" }}>Loading collections...</p>}
       </>
       :
       <><ul style={{ display: "table" }}>
