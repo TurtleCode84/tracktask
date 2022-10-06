b5521d36d7741ca14e67935372a20e1e82fe0b7c
Ankit_Anmol <87054411+Quantum-Codes@users.noreply.github.com>
2022-10-06 18:59:00 +0530

    Revert

--- pages/loading.js
+++ pages/loading.js
@@ -1,12 +1,10 @@
 import React from "react";
+import Layout from "components/Layout";
 import Loading from "components/Loading";
 import Image from "next/image";
 
 export default function Loader() {
   return (
-    <Layout>
-    <p>where do I exist?</p>
     <Loading/>
-    </Layout>
   );
 }
