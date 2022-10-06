732427f6c3dbd545840da891f894c24c429cd076
Ankit_Anmol <87054411+Quantum-Codes@users.noreply.github.com>
2022-10-06 18:16:10 +0530

    Indent

--- components/Loading.js
+++ components/Loading.js
@@ -10,41 +10,41 @@ export default function Loading() {
       </div>
     </Layout>
     <style jsx>{`
-#loading {
-  display: flex;
-  margin: 50px auto;
-  width: 8em;
-}
+      #loading {
+        display: flex;
+        margin: 50px auto;
+        width: 8em;
+      }
 
-@keyframes move {
-  0%, 100% {
-    transform: translate(0, 0px);
-    animation-timing-function: ease-out;
-  }
-  50% {
-    transform: translate(0, -25px);
-    animation-timing-function: ease-in;
-  }
-}
+      @keyframes move {
+        0%, 100% {
+          transform: translate(0, 0px);
+          animation-timing-function: ease-out;
+        }
+        50% {
+          transform: translate(0, -25px);
+          animation-timing-function: ease-in;
+        }
+      }
 
-.dot {
-  width: 1em;
-  height: 1em;
-  background: grey;
-  border-radius: 50%;
-  margin: 0.25em;
-  animation: move 1.2s linear infinite;
-}
+      .dot {
+        width: 1em;
+        height: 1em;
+        background: grey;
+        border-radius: 50%;
+        margin: 0.25em;
+        animation: move 1.2s linear infinite;
+      }
 
-.a {
-  animation-delay: 0.2s;
-}
-.b {
-  animation-delay: 0.4s;
-}
-.c {
-  animation-delay: 0.6s;
-}
+      .a {
+        animation-delay: 0.2s;
+      }
+      .b {
+        animation-delay: 0.4s;
+      }
+      .c {
+        animation-delay: 0.6s;
+      }
     `}
     </style></>
   );
