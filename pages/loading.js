212fe708bf215402e152f56e80ddc7560886c5d4
Ankit_Anmol <87054411+Quantum-Codes@users.noreply.github.com>
2022-10-06 18:32:34 +0530

    Create loading.js

---
+++ pages/loading.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "components/Layout";
+import Loading from "components/Loading";
+import Image from "next/image";
+
+export default function About() {
+  return (
+    <Layout>
+      An easteregg maybe?
+      <Loading/>
+    </Layout>
+  );
+}
