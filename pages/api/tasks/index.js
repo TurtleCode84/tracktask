7f86bc54a9bce0531ebcfd7b129d6a66bf560b3d
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-07 08:57:49 -0400

    Recent

--- pages/api/tasks/index.js
+++ pages/api/tasks/index.js
@@ -30,7 +30,7 @@ async function tasksRoute(req, res) {
     var data;
     if (collections !== "true") {
       if (filter === "recent") {
-        query.created = {$lte: (Math.floor(Date.now()/1000) - 86400)};
+        query.created = {$gte: (Math.floor(Date.now()/1000) - 86400)};
       } else if (filter === "upcoming") {
         query.dueDate = {$gt: Math.floor(Date.now()/1000)};
       } else if (filter === "overdue") {
