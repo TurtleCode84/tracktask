50c5f9d11d3e9ab2959d89d94115ffab805b7ac3
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-10 18:38:11 -0400

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -64,10 +64,10 @@ export default function Layout({ children }) {
         .material-symbols-outlined {
           font-color: #006dbe;
           font-variation-settings:
-            'FILL' 1,
+            'FILL' 0,
             'wght' 400,
             'GRAD' 0,
-            'opsz' 20
+            'opsz' 48
         }
       `}</style>
       {process.env.VERCEL_ENV !== "production" && <nav style={{backgroundColor: "orange", textAlign: "center", height:40 + "px", fontWeight: 600, marginTop: -20 + "px", paddingBottom: 5 + "px", minHeight: "min-content"}}><p style={{paddingTop: 13 + "px"}}>You are viewing a TrackTask Development release, some features may not work properly or may break your account.</p></nav>}
