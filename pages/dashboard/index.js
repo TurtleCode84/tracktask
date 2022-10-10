50d647ef40483e02cf52b89f294ae263d046b6f4
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-10 19:05:55 -0400

    Update index.js

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -29,7 +29,7 @@ export default function Dashboard() {
   const { tasks: collections, error: collectionsError } = useTasks(user, true, false);
   const collectionList = collections?.map((collection) =>
     <li key={collection._id} className="list-hover" style={{ margin: "0.5em", background: "#f8f8f8", padding: "5px", borderWidth: "2px", borderStyle: "solid", borderColor: "darkgray", borderRadius: "10px", width: "auto" }} onClick={() => router.push(`/collections/${collection._id}`)}>
-      {collection.owner === user.id ? <span title="You own this collection" style={{ color: "midnightblue" }} className="material-symbols-outlined">person</span> : null}{' '}<b>{collection.name}</b> - {collection.description.slice(0,25).trim()}... (created <DueDate timestamp={collection.created}/>)
+      {collection.owner === user.id ? <span title="You own this collection" style={{ color: "steelblue" }} className="material-symbols-outlined">person</span> : null}{' '}<b>{collection.name}</b> - {collection.description.slice(0,25).trim()}... (created <DueDate timestamp={collection.created}/>)
     </li>
   );
   
