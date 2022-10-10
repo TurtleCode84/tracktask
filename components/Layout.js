8210862f0920d0deb866f46f3c181a68a522822a
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-10 17:21:54 -0400

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -7,7 +7,7 @@ export default function Layout({ children }) {
     <>
       <Head>
         <title>TrackTask - Shareable Task Management</title>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
       </Head>
       <style jsx global>{`
         *,
@@ -62,11 +62,12 @@ export default function Layout({ children }) {
         }
         
         .material-symbols-outlined {
+          font-color: #006dbe;
           font-variation-settings:
-          'FILL' 1,
-          'wght' 400,
-          'GRAD' 0,
-          'opsz' 58
+            'FILL' 1,
+            'wght' 400,
+            'GRAD' 0,
+            'opsz' 68;
         }
       `}</style>
       {process.env.VERCEL_ENV !== "production" && <nav style={{backgroundColor: "orange", textAlign: "center", height:40 + "px", fontWeight: 600, marginTop: -20 + "px", paddingBottom: 5 + "px", minHeight: "min-content"}}><p style={{paddingTop: 13 + "px"}}>You are viewing a TrackTask Development release, some features may not work properly or may break your account.</p></nav>}
