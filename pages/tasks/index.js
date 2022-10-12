6e4a82615c49a5dc4693cd03d24178436df8c97f
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-11 21:41:28 -0400

    Replace emojis with icons

--- pages/tasks/index.js
+++ pages/tasks/index.js
@@ -17,7 +17,7 @@ export default function Tasks() {
   const router = useRouter();
   const taskList = allTasks?.map((task) =>
     <li key={task._id} className="list-hover" style={{ margin: "0.5em", background: "#f8f8f8", padding: "5px", borderWidth: "2px", borderStyle: "solid", borderColor: "darkgray", borderRadius: "10px", width: "auto" }} onClick={() => router.push(`/tasks/${task._id}`)}>
-      {task.completion.completed !== 0 ? <><span title="Completed">&#9989;{' '}</span></> : null}{task.priority ? <><span title="Priority">&#10071;</span></> : null}<b>{task.name}</b> - {task.description.slice(0,25).trim()}... (due <DueDate timestamp={task.dueDate}/>{task.dueDate !== 0 ? <>, on {moment.unix(task.dueDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}</> : null})
+      {task.completion.completed !== 0 ? <span title="Completed" style={{ color: "darkgreen" }} className="material-symbols-outlined icon-list">task_alt</span> : null}{task.priority ? <span title="Priority" style={{ color: "red" }} className="material-symbols-outlined icon-list">priority_high</span> : null}{' '}<b>{task.name}</b> - {task.description.slice(0,25).trim()}... (due <DueDate timestamp={task.dueDate}/>{task.dueDate !== 0 ? <>, on {moment.unix(task.dueDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}</> : null})
     </li>
   );
   
