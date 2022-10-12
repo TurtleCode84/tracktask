bedbe24e802f1713d41e466cddadc9ff8dbe96ac
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-11 21:13:32 -0400

    Styling

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -14,13 +14,13 @@ export default function Admin() {
   const { users: recentlyActive } = useAdminUsers(user, "login", 5);
   const activeUsersList = recentlyActive?.map((activeUser) =>
     <li key={activeUser._id} style={{ margin: "0.5em" }}>
-      <Link href={`/admin/users/${activeUser._id}`}>{activeUser.username}</Link>{activeUser.permissions.verified ? <span title="Verified" style={{ color: "#006dbe" }} className="material-symbols-outlined">verified</span> : null}{activeUser.permissions.admin ? <span title="Admin" style={{ color: "slategray" }} className="material-symbols-outlined">verified_user</span> : null}{activeUser.permissions.banned ? <span title="Banned" style={{ color: "red" }} className="material-symbols-outlined">block</span> : null} - Last login {activeUser.history.lastLogin > 0 ? moment.unix(activeUser.history.lastLogin).fromNow() : 'never'}
+      <Link href={`/admin/users/${activeUser._id}`}>{activeUser.username}</Link>{activeUser.permissions.verified ? <>{' '}<span title="Verified" style={{ color: "#006dbe" }} className="material-symbols-outlined icon-list">verified</span></> : null}{activeUser.permissions.admin ? <>{' '}<span title="Admin" style={{ color: "slategray" }} className="material-symbols-outlined icon-list">verified_user</span></> : null}{activeUser.permissions.banned ? <>{' '}<span title="Banned" style={{ color: "red" }} className="material-symbols-outlined icon-list">block</span></> : null} - Last login {activeUser.history.lastLogin > 0 ? moment.unix(activeUser.history.lastLogin).fromNow() : 'never'}
     </li>
   );
   const { users: recentlyJoined } = useAdminUsers(user, "joined", 5);
   const newUsersList = recentlyJoined?.map((newUser) =>
     <li key={newUser._id} style={{ margin: "0.5em" }}>
-      <Link href={`/admin/users/${newUser._id}`}>{newUser.username}</Link>{newUser.permissions.verified ? <span title="Verified" style={{ color: "#006dbe" }} className="material-symbols-outlined">verified</span> : null}{newUser.permissions.admin ? <span title="Admin" style={{ color: "slategray" }} className="material-symbols-outlined">verified_user</span> : null}{newUser.permissions.banned ? <span title="Banned" style={{ color: "red" }} className="material-symbols-outlined">block</span> : null} - Joined {newUser.history.joined > 0 ? moment.unix(newUser.history.joined).fromNow() : 'never'}
+      <Link href={`/admin/users/${newUser._id}`}>{newUser.username}</Link>{newUser.permissions.verified ? <>{' '}<span title="Verified" style={{ color: "#006dbe" }} className="material-symbols-outlined icon-list">verified</span></> : null}{newUser.permissions.admin ? <>{' '}<span title="Admin" style={{ color: "slategray" }} className="material-symbols-outlined icon-list">verified_user</span></> : null}{newUser.permissions.banned ? <>{' '}<span title="Banned" style={{ color: "red" }} className="material-symbols-outlined icon-list">block</span></> : null} - Joined {newUser.history.joined > 0 ? moment.unix(newUser.history.joined).fromNow() : 'never'}
     </li>
   );
 
