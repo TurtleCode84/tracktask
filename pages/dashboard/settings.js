cc9d0e3f7d7eb319ceaaf5a0448d52ff96a788ae
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-13 08:24:18 -0400

    Update settings.js

--- pages/dashboard/settings.js
+++ pages/dashboard/settings.js
@@ -21,7 +21,42 @@ export default function Settings() {
   return (
     <Layout>
       <h1>Edit your account:</h1>
-      <p style={{ fontStyle: "italic" }}>Page coming soon...</p>
+      <UserAdminForm
+          errorMessage={errorMsg}
+          user={user}
+          onSubmit={async function handleSubmit(event) {
+            event.preventDefault();
+            document.getElementById("editUserBtn").disabled = true;
+            if (event.currentTarget.password.value !== event.currentTarget.cpassword.value) {
+              setErrorMsg("New passwords do not match!");
+              document.getElementById("editUserBtn").disabled = false;
+              return;
+            }
+              
+            const body = {};
+            if (event.currentTarget.username.value !== event.currentTarget.username.defaultValue) {body.username = event.currentTarget.username.value}
+            if (event.currentTarget.email.value !== event.currentTarget.email.defaultValue) {body.email = event.currentTarget.email.value}
+            if (event.currentTarget.password.value) {body.password = event.currentTarget.password.value}
+            if (event.currentTarget.resetShareKey.checked) {body.resetShareKey = event.currentTarget.resetShareKey.checked}
+            if (event.currentTarget.profilePicture.value !== event.currentTarget.profilePicture.defaultValue) {body.profilePicture = event.currentTarget.profilePicture.value}
+
+            try {
+              await fetchJson(`/api/user`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(body),
+              })
+              router.reload();
+            } catch (error) {
+              if (error instanceof FetchError) {
+                setErrorMsg(error.data.message);
+              } else {
+                console.error("An unexpected error happened:", error);
+              }
+              document.getElementById("editUserBtn").disabled = false;
+            }
+          }}
+      />
     </Layout>    
   );
 }
