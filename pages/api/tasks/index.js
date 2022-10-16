685cb6bae08b40a80cf60e9259cacf88b309838f
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-15 20:56:25 -0400

    That apparently did nothing

--- pages/api/tasks/index.js
+++ pages/api/tasks/index.js
@@ -40,9 +40,9 @@ async function tasksRoute(req, res) {
       }
       try {
         data = await db.collection("tasks").find(query, taskoptions).toArray();
-        /*if (filter === "upcoming" || filter === "overdue") {
+        if (filter === "upcoming" || filter === "overdue") {
           data = data.filter(task => task.dueDate !== 0);
-        }*/
+        }
         if (filter === "upcoming" || filter === "overdue" || filter === "notdue") {
           data = data.filter(task => task.completion.completed === 0);
         }
