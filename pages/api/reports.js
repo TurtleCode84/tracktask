5ff99e8564c752f74c49b140dd4c8e705a49addd
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-18 10:43:43 -0400

    Create reports.js

---
+++ pages/api/reports.js
@@ -0,0 +1,22 @@
+import clientPromise from "lib/mongodb";
+import { hash } from 'bcryptjs';
+import { withIronSessionApiRoute } from "iron-session/next";
+import { sessionOptions } from "lib/session";
+import fetchJson from "lib/fetchJson";
+
+export default withIronSessionApiRoute(reportsRoute, sessionOptions);
+
+async function reportsRoute(req, res) {
+  const user = req.session.user;
+  if (req.method === 'GET' && user.permissions.admin) {
+    res.status(418).json({ message: "Under construction" });
+    return;
+  } else if (req.method === 'POST') {
+    //const { username, password, email } = await req.body;
+    res.status(418).json({ message: "Under construction" });
+    return;
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+    return;
+  }
+}
