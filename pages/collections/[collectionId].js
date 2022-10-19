543cb77401fdbf1065a6b9d2c7ee5b45834fa190
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-19 10:19:50 -0400

    Remove unneeded component

--- pages/collections/[collectionId].js
+++ pages/collections/[collectionId].js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Layout from "components/Layout";
 import Loading from "components/Loading";
-import DueDate from "components/DueDate";
 //import CollectionEditForm from "components/CollectionEditForm";
 import useUser from "lib/useUser";
 import useTasks from "lib/useTasks";
@@ -26,7 +25,7 @@ export default function Collection() {
   }
   const taskList = collection?.tasks.map((task) =>
     <li key={task._id} title={task.dueDate !== 0 ? moment.unix(task.dueDate).format("dddd, MMMM Do YYYY, h:mm:ss a") : 'No due date'} className="list-hover" style={{ margin: "0.5em", background: "#f8f8f8", padding: "5px", borderWidth: "2px", borderStyle: "solid", borderColor: "darkgray", borderRadius: "10px", width: "auto" }} onClick={() => router.push(`/tasks/${task._id}`)}>
-      {task.completion.completed !== 0 ? <span title="Completed" style={{ color: "darkgreen" }} className="material-symbols-outlined icon-list">task_alt</span> : null}{task.priority ? <span title="Priority" style={{ color: "red" }} className="material-symbols-outlined icon-list">priority_high</span> : null}{' '}<b>{task.name}</b> - {task.description.slice(0,30).trim()}... (due <DueDate timestamp={task.dueDate}/>)
+      {task.completion.completed !== 0 ? <span title="Completed" style={{ color: "darkgreen" }} className="material-symbols-outlined icon-list">task_alt</span> : null}{task.priority ? <span title="Priority" style={{ color: "red" }} className="material-symbols-outlined icon-list">priority_high</span> : null}{' '}<b>{task.name}</b> - {task.description.slice(0,30).trim()}... (due {task.dueDate !== 0 ? moment.unix(task.dueDate).fromNow() : 'never'})
     </li>
   );
   
