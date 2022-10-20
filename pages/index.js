f1046bba411f12c2178de1c63944a87929a1ebef
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-20 09:40:01 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
   }
   return (
     <Layout>
-      <h1 style={{ fontSize: 50, marginBottom: "-20px" }}>
+      <h1 style={{ fontSize: 50, marginBottom: "-20px", marginTop: "-2px" }}>
         <span style={{ marginRight: "-1.3rem", verticalAlign: "middle" }}>
           <Image src="/tracktask.png" width="304.7" height="110" priority="true" quality="90" alt="TrackTask" /> {/* Ratio 2.77:1 */}
         </span>
