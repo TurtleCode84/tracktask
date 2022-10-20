35f8590edab4f6a820a5a9bd58ae9774855ffd5b
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-19 21:05:25 -0400

    Disable this particular eslint warning

--- components/Layout.js
+++ components/Layout.js
@@ -7,6 +7,7 @@ export default function Layout({ children }) {
     <>
       <Head>
         <title>TrackTask - Shareable Task Management</title>
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap" />
       </Head>
       <style jsx global>{`
