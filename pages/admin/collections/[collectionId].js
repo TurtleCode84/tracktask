73423fed19785d9d318f59cad66a3821084e1ddd
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-23 17:47:23 -0400

    Improvements

--- pages/admin/collections/[collectionId].js
+++ pages/admin/collections/[collectionId].js
@@ -21,10 +21,6 @@ export default function Collection() {
   
   const [errorMsg, setErrorMsg] = useState("");
   const collection = collections?.[0];
-  var clientError;
-  /*if (collections && !collection) {
-    clientError = "No collection found";
-  }*/
   const taskList = collection?.tasks.map((task) =>
     <Task task={task} key={task._id}/>
   );
@@ -37,14 +33,14 @@ export default function Collection() {
   
   return (
     <Layout>
-      <h2>{collection?.sharing.shared ? <span title="Shared" style={{ color: "lightslategray" }} className="material-symbols-outlined">group</span> : <span title="Private" style={{ color: "lightslategray" }} className="material-symbols-outlined">lock</span>}{' '}{collection ? <>{collection.name}:</> : 'Loading...'}</h2>
+      <h2>{collection?.hidden ? <span title="Hidden" style={{ color: "red" }} className="material-symbols-outlined">disabled_visible</span> : null}{collection?.sharing.shared ? <span title="Shared" style={{ color: "lightslategray" }} className="material-symbols-outlined">group</span> : <span title="Private" style={{ color: "lightslategray" }} className="material-symbols-outlined">lock</span>}{' '}{collection ? <>{collection.name}:</> : 'Loading...'}</h2>
       <Link href="/dashboard">Back to dashboard</Link><br/>
       {collection ?
         <><h3>General information</h3>
         <p>Description: {collection.description}</p>
         <p title={moment.unix(collection.created).format("dddd, MMMM Do YYYY, h:mm:ss a")}>Created: {collection.created > 0 ? <>{moment.unix(collection.created).format("dddd, MMMM Do YYYY, h:mm:ss a")}{' '}({moment.unix(collection.created).fromNow()})</> : 'never'}</p>
-        <p>Hidden: {collection.hidden ? <span style={{ color: "darkgreen" }} className="material-symbols-outlined icon-list">check</span> : <span style={{ color: "red" }} className="material-symbols-outlined icon-list">close</span>}</p>
-        <p>Status: {collection.sharing.shared ? <span title="Shared" style={{ color: "lightslategray" }} className="material-symbols-outlined">group</span> : <span title="Private" style={{ color: "lightslategray" }} className="material-symbols-outlined">lock</span>}</p>
+        <p>Owner: {collection.owner}</p>
+        {collection.sharing.shared ? <p>Shared with: {/*sharedWithList*/}</p> : null}
         <p>Number of tasks: {collection.tasks.length}</p>
         <p>Tasks in collection:</p>
         {taskList === undefined || error ?
@@ -90,7 +86,7 @@ export default function Collection() {
         />
         </details>*/}</>
       :
-        <>{error || clientError ? <p>{clientError ? clientError : error.data.message}</p> : <p style={{ fontStyle: "italic" }}>Loading collection...</p>}</>
+        <>{error ? <p>{error.data.message}</p> : <p style={{ fontStyle: "italic" }}>Loading collection...</p>}</>
       }
       <br/><details>
         <summary>View raw JSON</summary>
