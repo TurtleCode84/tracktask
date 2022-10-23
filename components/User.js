134cd5941e032f7257b7cecdd12f7749eecefaa4
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-23 19:11:38 -0400

    Fix

--- components/User.js
+++ components/User.js
@@ -1,10 +1,10 @@
-import moment from "moment";
 import useTool from "lib/useTool";
+import Image from "next/Image";
 
 const { info, error } = useTool(user, "userInfo", id);
 
 export default function User({ user, id }) {
   return (
-    <span style={{ borderRadius: "100%", overflow: "hidden", marginRight: ".3em", verticalAlign: "middle" }}><Image src={info?.profilePicture ? info.profilePicture : "/default-pfp.jpg" } width={32} height={32} alt=""/></span>{info?.username}
+    <><span style={{ borderRadius: "100%", overflow: "hidden", marginRight: ".3em", verticalAlign: "middle" }}><Image src={info?.profilePicture ? info.profilePicture : "/default-pfp.jpg" } width={32} height={32} alt=""/></span>{info?.username}</>
   );
 }
