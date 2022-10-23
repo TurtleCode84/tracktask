d2575994e25e42f225ce771149787b3e699984c3
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-23 19:13:25 -0400

    Fix that

--- components/User.js
+++ components/User.js
@@ -1,9 +1,8 @@
 import useTool from "lib/useTool";
 import Image from "next/Image";
 
-const { info, error } = useTool(user, "userInfo", id);
-
 export default function User({ user, id }) {
+  const { info, error } = useTool(user, "userInfo", id);
   return (
     <><span style={{ borderRadius: "100%", overflow: "hidden", marginRight: ".3em", verticalAlign: "middle" }}><Image src={info?.profilePicture ? info.profilePicture : "/default-pfp.jpg" } width={32} height={32} alt=""/></span>{info?.username}</>
   );
