ee8fe5d7a0dc0a5a112e3adacc2190319fac5aac
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-23 15:14:31 -0400

    More functionality

--- pages/api/admin/collections/[[...id]].js
+++ pages/api/admin/collections/[[...id]].js
@@ -3,41 +3,32 @@ import { sessionOptions } from "lib/session";
 import { ObjectId } from 'mongodb'
 import clientPromise from "lib/mongodb";
 
-export default withIronSessionApiRoute(adminTaskRoute, sessionOptions);
+export default withIronSessionApiRoute(adminCollectionRoute, sessionOptions);
 
-async function adminTaskRoute(req, res) {
+async function adminCollectionRoute(req, res) {
   const user = req.session.user;
   if (!user || !user.isLoggedIn || !user.permissions.admin ) {
     res.status(401).json({ message: "Unauthorized" });
     return;
   }
-  const { id, collection } = req.query
-  if (!ObjectId.isValid(id)) {
-    res.status(422).json({ message: "Invalid object ID" });
+  const { id } = req.query
+  if (id && !ObjectId.isValid(id)) {
+    res.status(422).json({ message: "Invalid collection ID" });
     return;
   }
   if (req.method === 'GET') {
     const client = await clientPromise;
     const db = client.db("data");
     const query = {
-      _id: ObjectId(id),
-      $or: [
-        { owner: ObjectId(user.id) },
-        { 'sharing.shared': true },
-      ],
+      'sharing.shared': true,
     };
+    if (id) {
+      query._id = ObjectId(id);
+    }
     try {
-      var getTasks;
-      if (collection !== "true") {
-        getTasks = await db.collection("tasks").findOne(query);
-      } else {
-        getTasks = await db.collection("collections").findOne(query);
-        getTasks.tasks = await db.collection("tasks").find({ _id: {$in: getTasks.tasks} }).toArray();
-      }
-      if (!getTasks && collection !== "true") {
-        res.status(404).json({ message: "No tasks found" });
-        return;
-      } else if (!getTasks) {
+      var getTasks = await db.collection("collections").findOne(query);
+      getTasks.tasks = await db.collection("tasks").find({ _id: {$in: getTasks.tasks} }).toArray();
+      if (!getTasks) {
         res.status(404).json({ message: "No collection found" });
         return;
       }
@@ -49,7 +40,9 @@ async function adminTaskRoute(req, res) {
     res.status(418).json({ message: "Under construction" });
     return;
   } else if (req.method === 'DELETE') {
-    const client = await clientPromise;
+    res.status(418).json({ message: "Under construction" });
+    return;
+    /*const client = await clientPromise;
     const db = client.db("data");
     var deletedItem;
     if (collection !== "true") {
@@ -57,7 +50,7 @@ async function adminTaskRoute(req, res) {
     } else {
       deletedItem = await db.collection("collections").deleteOne({ _id: ObjectId(id) });
     }
-    res.json(deletedItem);
+    res.json(deletedItem);*/
   } else {
     res.status(405).json({ message: "Method not allowed" });
     return;
