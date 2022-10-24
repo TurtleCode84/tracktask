3b4c0c683218732f08c77e3f11c81255c95a779b
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-23 20:11:41 -0400

    Update User.js

--- components/User.js
+++ components/User.js
@@ -1,9 +1,10 @@
 import useTool from "lib/useTool";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function User({ user, id }) {
+export default function User({ user, id, link }) {
   const { info, error } = useTool(user, "userInfo", id);
   return (
-    <><span style={{ borderRadius: "100%", overflow: "hidden", marginRight: ".3em", verticalAlign: "middle" }}><Image src={info?.profilePicture ? info.profilePicture : "/default-pfp.jpg" } width={32} height={32} alt=""/></span>{info?.username}</>
+    <><span style={{ borderRadius: "100%", overflow: "hidden", marginRight: ".3em", verticalAlign: "middle" }}><Image src={info?.profilePicture ? info.profilePicture : "/default-pfp.jpg" } width={32} height={32} alt=""/></span>{link ? <Link href={`/admin/users/${info?._id}`}>{info?.username}</Link> : info?.username}</>
   );
 }
