e6c63f83c14e31a75858ed1699c7faa5146e2716
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-25 12:22:55 -0400

    Update User.js

--- components/User.js
+++ components/User.js
@@ -4,6 +4,6 @@ import Image from "next/image";
 export default function User({ user, id, link }) {
   const { info, error } = useTool(user, "userInfo", id);
   return (
-    <><span style={{ borderRadius: "100%", overflow: "hidden", marginRight: ".3em", verticalAlign: "middle" }}><Image src={info?.profilePicture ? info.profilePicture : "/default-pfp.jpg" } width={32} height={32} alt=""/></span>{link ? <a href={`/admin/users/${info?._id}`}>{info?.username}</a> : info?.username}</>
+    <><span style={{ borderRadius: "100%", overflow: "hidden", marginRight: ".3em", verticalAlign: "middle" }}><Image src={info?.profilePicture ? info.profilePicture : "/default-pfp.jpg" } width={32} height={32} alt=""/></span>{link ? <a href={`/admin/users/${info?._id}`}>{info?.username}</a> : info?.username}{info.permissions.verified && <>{' '}<span title="Verified" style={{ color: "#006dbe" }} className="material-symbols-outlined icon-list">verified</span></>}</>
   );
 }
