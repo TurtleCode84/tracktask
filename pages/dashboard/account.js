0a49908c430acfcad2ae17bb2f20c26521afee90
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-25 18:04:31 -0400

    Update account.js

--- pages/dashboard/account.js
+++ pages/dashboard/account.js
@@ -78,7 +78,7 @@ export default function Settings() {
            }}
         />
       </details>
-      <p style={{ fontStyle: "italic" }} title={moment.unix(user.lastEdit?.timestamp).format("dddd, MMMM Do YYYY, h:mm:ss a")}>Last edited {user.lastEdit?.timestamp ? moment.unix(user.lastEdit?.timestamp).fromNow() : 'never'}{user.lastEdit?.by !== user.id && ' by an administrator'}</p>
+      <p style={{ fontStyle: "italic" }} title={user.lastEdit?.timestamp > 0 ? moment.unix(user.lastEdit?.timestamp).format("dddd, MMMM Do YYYY, h:mm:ss a") : 'Never'}>Last edited {user.lastEdit?.timestamp > 0 ? moment.unix(user.lastEdit?.timestamp).fromNow() : 'never'}{user.lastEdit?.by && user.lastEdit?.by !== user.id && ' by an administrator'}</p>
     </Layout>    
   );
 }
