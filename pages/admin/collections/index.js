28ac75bfa72161a54cd60cb60710d6c21b4c5ba8
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-10-25 20:37:45 -0400

    Copyedit

---
+++ pages/admin/collections/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import moment from "moment";
+import Layout from "components/Layout";
+import Loading from "components/Loading";
+import Collection from "components/Collection";
+import Link from "next/link";
+import useUser from "lib/useUser";
+import useAdminUsers from "lib/useAdminUsers";
+
+export default function CollectionsAdmin() {
+  const { user } = useUser({
+    redirectTo: "/login",
+    adminOnly: true,
+  });
+  const { collections, error: collectionsError } = useAdminCollections(user, false);
+  const collectionList = collections?.map((collection) =>
+    <Collection collection={collection} key={collection._id}/>
+  );
+  
+  if (!user || !user.isLoggedIn || !user.permissions.admin) {
+    return (
+      <Loading/>
+    );
+  }
+  return (
+    <Layout>
+      <h1>All shared collections:</h1>
+      <Link href="/admin">Back to admin dashboard</Link><br/>
+      {collectionList === undefined || collectionsError ?
+      <>
+      {collectionsError ? <p style={{ fontStyle: "italic" }}>{collectionsError.data ? collectionsError.data.message : collectionsError.message}</p> : <p style={{ fontStyle: "italic" }}>Loading tasks...</p>}
+      </>
+      :
+      <><ul style={{ display: "table" }}>
+        {collectionList}
+      </ul></>
+      }
+    </Layout>    
+  );
+}
