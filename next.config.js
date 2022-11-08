30c5fb209e7b097022c47a960bdc6f3a4bca28ff
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-11-08 23:58:03 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,11 +3,4 @@ module.exports = {
   images: {
     domains: ["tracktask.eu.org", "avatars.githubusercontent.com", "u.cubeupload.com", "i.bb.co", "api.wasteof.money"],
   },
-  async redirects() {
-    return [
-      process.env.MAINTENANCE_MODE === "true"
-        ? { source: "/((?!maintenance)(?!_next)(?!static).*)", destination: "/maintenance", permanent: false }
-        : null,
-    ].filter(Boolean);
-  },
 };
