6332ec0ac7f6f33c8a9bac5616048fb17e176890
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-11-08 23:56:20 +0000

    Maintenance mode?

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ module.exports = {
   images: {
     domains: ["tracktask.eu.org", "avatars.githubusercontent.com", "u.cubeupload.com", "i.bb.co", "api.wasteof.money"],
   },
+  async redirects() {
+    return [
+      process.env.MAINTENANCE_MODE === "true"
+        ? { source: "/((?!maintenance)(?!_next)(?!static).*)", destination: "/maintenance", permanent: false }
+        : null,
+    ].filter(Boolean);
+  },
 };
