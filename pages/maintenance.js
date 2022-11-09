f1321a7c26a4dce19e3196fcb2b49252603d8f4c
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-11-08 20:11:34 -0500

    Fix xss

--- pages/maintenance.js
+++ pages/maintenance.js
@@ -15,7 +15,7 @@ export default function Maintenance() {
             <h1 style="font-size: 50; padding-top: 5px; line-height: 0px">
                 <span style="color: #006dbe" class="material-symbols-outlined">engineering</span> Maintenance In Progress
             </h1>
-            <h3 style="font-size: 20; line-height: 0px">We're working on improving your user experience, TrackTask will be back online as soon as possible.</h3>
+            <h3 style="font-size: 20; line-height: 0px">We&apos;re working on improving your user experience, TrackTask will be back online as soon as possible.</h3>
             <p style="line-height: 45px">Thank you for your patience & cooperation!</p>
         </Layout>
     );
