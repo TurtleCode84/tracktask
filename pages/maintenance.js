aa6defd1552dcf028790fdb761570c8f0de3a7db
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-11-08 21:07:32 -0500

    Test fix

--- pages/maintenance.js
+++ pages/maintenance.js
@@ -3,7 +3,7 @@ import Layout from "components/Layout";
 import Loading from "components/Loading";
 
 export default function Maintenance() {
-    if (process.env.MAINTENANCE !== "true") {
+    if (process.env.NEXT_PUBLIC_MAINTENANCE !== "true") {
         return (
             <Loading />
         );
