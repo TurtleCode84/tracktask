35067d145c144d7a51564543e46905fdb7a4406d
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2022-11-09 08:44:23 -0500

    Test

--- components/Layout.js
+++ components/Layout.js
@@ -82,7 +82,7 @@ export default function Layout({ children }) {
         }
       `}</style>
       {process.env.NEXT_PUBLIC_VERCEL_ENV !== "production" && <nav style={{backgroundColor: "orange", textAlign: "center", height:40 + "px", fontWeight: 600, marginTop: -20 + "px", paddingBottom: 5 + "px", minHeight: "min-content"}}><p style={{paddingTop: 13 + "px"}}>You are viewing a TrackTask Development release, some features may not work properly or may break your account.</p></nav>}
-      <nav style={{backgroundColor: advisoryColor, textAlign: "center", height:40 + "px", fontWeight: 600, marginTop: -20 + "px", paddingBottom: 5 + "px", minHeight: "min-content"}}><p style={{paddingTop: 13 + "px"}}>{process.env.NEXT_PUBLIC_ADVISORY?.split(',')[1]}</p></nav>
+      <nav style={{backgroundColor: advisoryColor, textAlign: "center", height:40 + "px", fontWeight: 600, marginTop: -20 + "px", paddingBottom: 5 + "px", minHeight: "min-content"}}><p style={{paddingTop: 13 + "px"}}>{process.env.ADVISORY?.split(',')[1]}</p></nav>
       <Header />
 
       <main>
