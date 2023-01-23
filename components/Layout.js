b8365f58cd33a8263ff4df154f4b59b71a53a5b7
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-01-23 15:00:45 -0500

    Class

--- components/Layout.js
+++ components/Layout.js
@@ -132,7 +132,7 @@ export default function Layout({ children }) {
           header {
             background-color: #222 !important;
           }
-          list-hover {
+          .list-hover {
             background-color: #111 !important;
             border-color: #333 !important;
           }
