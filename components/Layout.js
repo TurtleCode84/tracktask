93f33e8a98babc4258391e0abe5f25947a0c146a
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-01-23 15:02:10 -0500

    Override

--- components/Layout.js
+++ components/Layout.js
@@ -142,7 +142,7 @@ export default function Layout({ children }) {
           }
         }
       `}</style>
-      {process.env.NEXT_PUBLIC_VERCEL_ENV !== "production" && <nav style={{backgroundColor: "orange", textAlign: "center", height:40 + "px", fontWeight: 600, marginTop: -20 + "px", paddingBottom: 5 + "px", minHeight: "min-content"}}><p style={{paddingTop: 13 + "px"}}>You are viewing a TrackTask Development release, some features may not work properly or may break your account.</p></nav>}
+      {process.env.NEXT_PUBLIC_VERCEL_ENV !== "production" && <nav style={{backgroundColor: "orange", color: "black !important", textAlign: "center", height:40 + "px", fontWeight: 600, marginTop: -20 + "px", paddingBottom: 5 + "px", minHeight: "min-content"}}><p style={{paddingTop: 13 + "px"}}>You are viewing a TrackTask Development release, some features may not work properly or may break your account.</p></nav>}
       {advisory && <nav style={{backgroundColor: advisoryColor, textAlign: "center", height:40 + "px", fontWeight: 600, marginTop: -20 + "px", paddingBottom: 5 + "px", minHeight: "min-content"}}><p style={{paddingTop: 13 + "px"}}>{advisory.split(',')[1]}</p></nav>}
       <Header />
 
