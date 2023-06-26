3f355855d8dc6c076db1f5db8a3783e2675c2a34
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-06-26 12:12:40 -0400

    Testing

--- pages/api/tasks.js
+++ pages/api/tasks.js
@@ -48,7 +48,8 @@ async function tasksRoute(req, res) {
         }
         const allCollections = await db.collection("collections").find(query).toArray();
         for (var i=0; i<data.length; i++) {
-          data[i].collections = allCollections.filter(allCollection => allCollection.tasks?.some((element) => element._id === data[i]._id))
+          data[i].collections = allCollections[0].name;
+          //data[i].collections = allCollections.filter(allCollection => allCollection.tasks?.some((element) => element._id === data[i]._id))
         }
       } catch (error) {
         res.status(200).json([]);
