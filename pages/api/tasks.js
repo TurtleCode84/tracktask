f18fc73bac4a7a752cbb304ed123016304cc3a61
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-06-26 13:10:26 -0400

    Strange

--- pages/api/tasks.js
+++ pages/api/tasks.js
@@ -54,7 +54,8 @@ async function tasksRoute(req, res) {
               //data[i].collections.push(allCollections[j].name);
               data[i].collections.push("x");
             }
-            data[i].collections.push(allCollections[j].tasks.filter(task => task == data[i]._id));
+            data[i].collections.push(data[i]._id);
+            //data[i].collections.push(allCollections[j].tasks.filter(task => task == data[i]._id));
           }
         }
       } catch (error) {
