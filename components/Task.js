8a6a891cfe89136fe72914508910da32fb81ba68
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-06-26 17:40:57 -0400

    Ugh, JSX

--- components/Task.js
+++ components/Task.js
@@ -5,7 +5,7 @@ import stringToColor from "lib/stringToColor";
 export default function Task({ task, key }) {
   const router = useRouter();
   const collectionTags = task.collections?.map((item, index) =>
-    <span key={index} style={`background-color: ${stringToColor(item)}; padding-top: 0.5px; padding-left: 4px; padding-bottom: 0.5px; padding-right: 4px; border-color: #d1d854; border-style: solid; border-width: 2px; border-radius: 10px; color: black; margin-top: 5px; margin-left: 20px; margin-right: -15px; display: inline-block;`}>{item}</span>
+    <span key={index} style={{backgroundColor: stringToColor(item), paddingTop: "0.5px", paddingLeft: 4 + "px", paddingBottom: "0.5px", paddingRight: 4 + "px", borderColor: "#d1d854", borderStyle: "solid", borderWidth: 2 + "px", borderRadius: 10 + "px", color: "black", marginTop: 5 + "px", marginLeft: 20 + "px", marginRight: -15 + "px", display: "inline-block" }}>{item}</span>
   );
   return (
     <li key={key} title={task.dueDate !== 0 ? moment.unix(task.dueDate).format("dddd, MMMM Do YYYY, h:mm:ss a") : 'No due date'} className="list-hover" style={{ margin: "0.5em", background: "#f8f8f8", padding: "8px", borderWidth: "2px", borderStyle: "solid", borderColor: "darkgray", borderRadius: "10px", width: "auto" }} onClick={() => router.push(`/tasks/${task._id}`)}>
