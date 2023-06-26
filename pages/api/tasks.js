d8810e7d804de5827bdc3ed5268b7ec3f8c24d96
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-06-26 18:28:02 -0400

    Breaking something to fix something else v2.0

--- pages/api/tasks.js
+++ pages/api/tasks.js
@@ -40,12 +40,12 @@ async function tasksRoute(req, res) {
       }
       try {
         data = await db.collection("tasks").find(query, taskoptions).toArray();
-        if (filter === "upcoming" || filter === "overdue") {
+        /*if (filter === "upcoming" || filter === "overdue") {
           data = data.filter(task => task.dueDate !== 0);
         }
         if (filter === "upcoming" || filter === "overdue" || filter === "notdue") {
           data = data.filter(task => task.completion.completed === 0);
-        }
+        }*/
         const allCollections = await db.collection("collections").find(query).toArray();
         data.collections = [];
         for (var i=0; i<data.length; i++) {
