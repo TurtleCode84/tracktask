b89c133b780cbc16c60e3f1d5bd7955e76f8dc94
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-06-26 15:27:29 -0400

    ???

--- pages/api/tasks.js
+++ pages/api/tasks.js
@@ -54,7 +54,8 @@ async function tasksRoute(req, res) {
               //data[i].collections.push(allCollections[j].name);
               data[i].collections.push("x");
             }
-            data[i].collections.push(allCollections[j].tasks.includes(new ObjectId(data[i]._id)));
+            data[i].collections.push(typeof allCollections[j].tasks);
+            data[i].collections.push(typeof data[i]._id);
             //data[i].collections.push(allCollections[j].tasks.filter(task => task == data[i]._id));
           }
         }
