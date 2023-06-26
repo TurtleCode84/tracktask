f4889ee84ab09c06a297835caf4e6c0341b067a4
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-06-26 16:18:22 -0400

    Debug

--- pages/api/tasks.js
+++ pages/api/tasks.js
@@ -56,7 +56,9 @@ async function tasksRoute(req, res) {
           data[i].collections = taskInCollection;
         }
       } catch (error) {
-        res.status(200).json([]);
+        //res.status(200).json([]);
+        //return;
+        res.status(500).json({ message: error.message });
         return;
       }
     } else {
