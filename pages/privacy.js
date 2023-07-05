7ac0fe88d221a258448f8f8b924f92412a9c7c67
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-07-05 12:25:27 -0400

    bruhhh

--- pages/privacy.js
+++ pages/privacy.js
@@ -21,7 +21,7 @@ export default function Privacy() {
       </ul>
       <p>The above information is collected to allow the site to function correctly & securely, and so our development team can provide fixes and new features for TrackTask users. For questions and concerns related to your privacy on TrackTask, please contact the email linked below.</p>
       <h3>We legally cannot collect information from users under 13 years of age without parental consent, if you are a user or parent of a user under 13, please contact <a href="mailto:privacy@tracktask.eu.org">privacy@tracktask.eu.org</a> to have your information removed.</h3>
-      <p style="font-style: italic">This page was last updated 7/5/23.</p>
+      <p style={{ fontStyle: "italic" }}>This page was last updated 7/5/23.</p>
 
       <style jsx>{`
         li {
