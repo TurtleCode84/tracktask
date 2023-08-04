71222c2ec4820c0b549c1abd4b825f7d4624b616
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-08-04 14:04:55 -0400

    Better debug

--- pages/api/notifications.js
+++ pages/api/notifications.js
@@ -22,15 +22,14 @@ async function notificationsRoute(req, res) {
       const usersOptions = { projection: { _id: 1, notifications: 1 } };
       // Due to API constraints, we can only notify task owners
       const users = await db.collection("users").find(usersQuery, usersOptions); // This gets verified, non-banned users that have notifications enabled
-      debug.users = users.length;
-      debug.tasks = [];
+      debug.users = users;
       const tasksOptions = {
         projection: { name: 1 },
       };
       for (var i=0; i<users.length; i++) {
         const tasksQuery = { hidden: false, owner: new ObjectId(users[i]._id), 'completion.completed': 0, dueDate: {$gt: 0}, dueDate: {$lte: Math.floor(Date.now()/1000)}, dueDate: {$gte: users[i].notifications.enabled}, notified: {$ne: true} };
         const tasks = await db.collection("tasks").find(tasksQuery, tasksOptions); // Now we should have all tasks eligible for notification in this particular user
-        debug.tasks.push(tasks.length);
+        debug.tasks = tasks;
         for (var j=0; j<tasks.length; j++) {
           webpush.sendNotification(users[i].notifications.subscription, "You have a task that is due, but we're too lazy to tell you what it is. Go check TrackTask yourself.");
           const notified = await db.collection("tasks").updateOne({ _id: new ObjectId(tasks[j]._id)}, { $set: {notified: true} });
