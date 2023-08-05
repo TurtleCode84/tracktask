2b41c056b98c505b8a8b012cb8927b30758e340f
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-08-05 17:51:25 -0400

    test

--- components/ErrorBoundary.js
+++ components/ErrorBoundary.js
@@ -1,17 +1,20 @@
 import React from "react";
 import Layout from "components/Layout";
+import { useRouter } from "next/router";
+
+const router = useRouter();
 
 class ErrorBoundary extends React.Component {
     constructor(props) {
       super(props)
    
       // Define a state variable to track whether is an error or not
-      this.state = { hasError: false }
+      this.state = { hasError: false, errorMsg: null }
     }
     static getDerivedStateFromError(error) {
       // Update state so the next render will show the fallback UI
    
-      return { hasError: true }
+      return { hasError: true, errorMsg: error.message }
     }
     componentDidCatch(error, errorInfo) {
       // You can use your own error logging service here
@@ -27,8 +30,13 @@ class ErrorBoundary extends React.Component {
             <h3>We&apos;re not sure why this happened, but the error has been reported and will be resolved as soon as possible.</h3>
             <p>In the meantime, you probably want to go back to <a href="" onClick={(e) => {
               e.preventDefault();
-              this.setState({ hasError: false });
+              this.setState({ hasError: false, errorMsg: error.message });
+              router.push("/");
             }}>a page that does work</a>?</p>
+            <details style={{ fontSize: "90%", color: "darkgray" }}>
+              <summary>More details</summary>
+              <textarea>{this.state.errorMsg}</textarea>
+            </details>
           </Layout>
         )
       }
