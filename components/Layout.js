71a29f738411964b5ea3796636cfc4d08261629f
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-08-15 20:55:22 -0400

    Unnecessary caps

--- components/Layout.js
+++ components/Layout.js
@@ -207,7 +207,7 @@ export default function Layout({ children }) {
           margin: 1rem 0 0;
         }
       `}</style>
-      {process.env.NEXT_PUBLIC_VERCEL_ENV !== "production" && <nav style={{backgroundColor: "orange", textAlign: "center", fontWeight: 600, marginTop: -20 + "px", height: "fit-content", paddingBottom: 0, marginBottom: -16 + "px"}}><p style={{paddingTop: 13 + "px", paddingBottom: 8 + "px"}}>You are viewing a TrackTask Development release, some features may not work properly or may break your account.</p></nav>}
+      {process.env.NEXT_PUBLIC_VERCEL_ENV !== "production" && <nav style={{backgroundColor: "orange", textAlign: "center", fontWeight: 600, marginTop: -20 + "px", height: "fit-content", paddingBottom: 0, marginBottom: -16 + "px"}}><p style={{paddingTop: 13 + "px", paddingBottom: 8 + "px"}}>You are viewing a TrackTask development release, some features may not work properly or may break your account.</p></nav>}
       {advisory && <nav style={{backgroundColor: advisoryColor, textAlign: "center", fontWeight: 600, marginTop: -20 + "px", height: "fit-content", paddingBottom: 0, marginBottom: -16 + "px"}}><p style={{paddingTop: 13 + "px", paddingBottom: 8 + "px"}}>{advisory.split(',')[1]}</p></nav>}
       <Header />
 
