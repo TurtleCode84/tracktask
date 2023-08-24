b20e832dae377a56dced7454b11eb5c828de5ae9
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-08-24 17:17:51 -0400

    Add stuff

--- pages/collections/[collectionId]/share.js
+++ pages/collections/[collectionId]/share.js
@@ -23,21 +23,43 @@ export default function CollectionShare() {
     <li key={item.id} style={{ paddingBottom: "5px" }}><User user={user} id={item.id}/> <span style={{ fontSize: "80%", fontStyle: "italic", color: "darkgray" }}>({item.role.split('-')[0]})</span></li>
   );
   
-  if (!user || !user.isLoggedIn || user.permissions.banned || !collection) {
+  if (!user || !user.isLoggedIn || user.permissions.banned || !collection) { // We need to know the collection details before we show the page
     return (
       <Loading/>
     );
   }
   return (
     <Layout>
-      {user?.permissions.verified && user?.id === collection?.owner ? <>
+      {user.permissions.verified && user.id === collection.owner ? <>
       <h1>Share settings for {collection ? <>&quot;{collection.name}&quot;</> : 'a collection'}:</h1>
       <p>Back to <Link href={`/collections/${collection?._id}`}>collection</Link> or <Link href="/dashboard">dashboard</Link></p>
-      {/*collection ?
-      <>*/}
       {collection.sharing.shared ?
       <><h3>Currently shared with:</h3>
-      <p><ul>{sharedWithList.length > 0 ? sharedWithList : <li>Nobody!</li>}</ul></p></>
+      <p><ul>{sharedWithList.length > 0 ? sharedWithList : <li>Nobody!</li>}</ul></p>
+      <a href={`/api/collections/${collection._id}`}
+        onClick={async (e) => {
+          e.preventDefault();
+          document.getElementById("disableSharingBtn").disabled = true;
+          const body = {
+            shared: false,
+          };
+          try {
+            await fetchJson(`/api/collections/${collection._id}`, {
+              method: "PATCH",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify(body),
+            })
+            router.reload();
+          } catch (error) {
+            if (error instanceof FetchError) {
+              setErrorMsg(error.data.message);
+            } else {
+              console.error("An unexpected error happened:", error);
+            }
+            document.getElementById("disableSharingBtn").disabled = false;
+          }
+        }}
+        ><button id="disableSharingBtn">Disable sharing <span style={{ color: "lightslategray" }} className="material-symbols-outlined icon-list">lock</span></button></a><hr/></>
       :
       <><p style={{ fontStyle: "italic" }}>Sharing is currently disabled for this collection.</p>
       <a href={`/api/collections/${collection._id}`}
@@ -63,7 +85,7 @@ export default function CollectionShare() {
             document.getElementById("enableSharingBtn").disabled = false;
           }
         }}
-        ><button id="enableSharingBtn">Enable sharing <span style={{ color: "lightslategray" }} className="material-symbols-outlined icon-list">group</span></button></a><br/></>
+        ><button id="enableSharingBtn">Enable sharing <span style={{ color: "lightslategray" }} className="material-symbols-outlined icon-list">group</span></button></a><hr/></>
       }
       <details><summary>Add a new user</summary><br/>
       <CollectionShareForm
@@ -93,12 +115,7 @@ export default function CollectionShare() {
             document.getElementById("shareCollectionBtn").disabled = false;
           }
         }}
-      /></details>
-      {/*</>
-      :
-        <>{error ? <p>{error.data.message}</p> : <p style={{ fontStyle: "italic" }}>Loading collection...</p>}</>
-      */}
-      </>
+      /></details></>
       :
       <><h1 style={{marginBottom: "0px", marginTop: "60px"}}><span style={{color: "lightslategray", fontSize: "inherit"}} className="material-symbols-outlined">lock</span> 403: You don&apos;t have access to this page.</h1>
       <h3>We&apos;re not sure how you got here, but you don&apos;t have permission to view this page. Maybe you aren&apos;t logged in?</h3>
