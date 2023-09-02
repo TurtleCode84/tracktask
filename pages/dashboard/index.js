409279a76c241acc4bd6680c7ddf7931e234d21f
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-09-02 12:10:43 -0400

    Border color fix

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -62,7 +62,7 @@ export default function Dashboard() {
 
       {dynamicMsg && <p className="success">{dynamicMsg}{' '}<Link href="/dashboard">Ok</Link></p>}
 
-      <div style={{ display: "grid", gridTemplateColumns: "3fr 2fr", gap: "40px", minHeight: "300px" }}><div style={{ position: "relative", padding: "5px", borderStyle: "solid", borderWidth: "2px", borderColor: "var(--inset-border-color)", borderRadius: "7px" }}>
+      <div style={{ display: "grid", gridTemplateColumns: "3fr 2fr", gap: "40px", minHeight: "300px" }}><div style={{ position: "relative", padding: "5px", borderStyle: "solid", borderWidth: "2px", borderColor: "var(--border-color)", borderRadius: "7px" }}>
       {(upcomingTaskList === undefined || overdueTaskList === undefined || notdueTaskList === undefined) && <p style={{ fontStyle: "italic" }}>Loading tasks...</p>}
       {(upcomingTasksError || overdueTasksError || notdueTasksError) && <p style={{ fontStyle: "italic" }}>An error occurred while loading your tasks.</p>}
       {(upcomingTaskList && upcomingTaskList.length === 0) && (overdueTaskList && overdueTaskList.length === 0) && (notdueTaskList && notdueTaskList.length === 0) &&
@@ -93,7 +93,7 @@ export default function Dashboard() {
       <Link href="/tasks" style={{ position: "absolute", bottom: 0, right: 0, paddingRight: "5px", paddingBottom: "5px" }}>View all tasks</Link>
       </div>
       
-      <div style={{ position: "relative", padding: "5px", borderStyle: "solid", borderWidth: "2px", borderColor: "var(--inset-border-color)", borderRadius: "7px" }}>
+      <div style={{ position: "relative", padding: "5px", borderStyle: "solid", borderWidth: "2px", borderColor: "var(--border-color)", borderRadius: "7px" }}>
       <h2 style={{ marginTop: 0 }}>Your collections:</h2>
       {collectionList === undefined && !collectionsError && <p style={{ fontStyle: "italic" }}>Loading collections...</p>}
       {collectionsError && <p style={{ fontStyle: "italic" }}>An error occurred while loading your collections.</p>}
