985fbb3668e2fddb86f27248d6aa2a548c9c72fe
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-09-05 23:12:34 -0400

    i swear if it was that simple

--- components/Layout.js
+++ components/Layout.js
@@ -96,8 +96,7 @@ export default function Layout({ children }) {
           .dashboard {
             grid-template-columns: 1fr;
             gap: 30px;
-            width: max-content;
-            max-width: 100vw;
+            width: 100vw;
           }
         }
         
