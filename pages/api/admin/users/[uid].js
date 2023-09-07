eceaf2213ea64dbb6355aa1772fb3cfaf5812ce7
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-09-06 21:09:17 -0400

    oops

--- pages/api/admin/users/[uid].js
+++ pages/api/admin/users/[uid].js
@@ -27,12 +27,13 @@ async function adminUserRoute(req, res) {
       if (getUser) {
         const countTasks = await db.collection("tasks").countDocuments({ owner: new ObjectId(getUser._id) });
         const countCollections = await db.collection("collections").countDocuments({ owner: new ObjectId(getUser._id) });
+        const countShared = countCollections.filter((collection) => collection.sharing.shared === true).length;
         res.json({
           ...getUser,
           stats: {
             tasks: countTasks,
             collections: countCollections,
-            //shared: countCollections.filter((collection) => collection.sharing.shared === true).length,
+            shared: countShared,
           },
         });
       } else {
