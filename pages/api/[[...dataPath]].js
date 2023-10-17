055340f28e0c9e7053e687c44b4561f19d04702f
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-10-17 13:36:15 -0400

    Debug

--- pages/api/[[...dataPath]].js
+++ pages/api/[[...dataPath]].js
@@ -664,7 +664,7 @@ async function dataRoute(req, res) {
               },
             }
           };
-          const updatedCollection = await db.collection("collections").updateOne(query, updateDoc);
+          //const updatedCollection = await db.collection("collections").updateOne(query, updateDoc);
           
           const updatedCollectionInfo = await db.collection("collections").findOne({ _id: new ObjectId(dataPath[1]) }, { projection: {tasks: 1} });
           const updatedCollectionTasks = await db.collection("tasks").find({ _id: {$in: [updatedCollectionInfo.tasks]} }, { projection: {owner: 1} }).toArray();
@@ -677,7 +677,10 @@ async function dataRoute(req, res) {
           });
 
           const removedTasks = await db.collection("collections").updateOne({ _id: new ObjectId(dataPath[1]) }, { $pull: {tasks: {$in: taskIds}} });
-          res.json(updatedCollection);
+          
+          res.status(418).json({ message: String({ one: updatedCollectionInfo, two: updatedCollectionTasks, three: taskIds, four: removedTasks }) });
+          return;
+          //res.json(updatedCollection);
 
         } else { // Removing self from collection
 
