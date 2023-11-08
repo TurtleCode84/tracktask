8ed036338f96a7ea4834a2418e6275ecc3e6e19c
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-11-07 21:58:05 -0500

    Email verification soon

--- components/CollectionCreateForm.js
+++ components/CollectionCreateForm.js
@@ -9,7 +9,7 @@ export default function CollectionCreateForm({ verified, errorMessage, onSubmit 
         <span>Description</span>
         <textarea name="description" rows="8" cols="30" maxlength="500" required />
       </label>
-      {verified ? <p style={{ fontStyle: "italic" }}>You will be able to share this collection after you create it.</p> : <br/>}
+      <p style={{ fontStyle: "italic" }}>{verified ? 'You will be able to share this collection after you create it.' : 'If you would like to share this collection, please verify your email.'}</p>
 
       <button type="submit" id="createCollectionBtn">Create collection</button>
 
