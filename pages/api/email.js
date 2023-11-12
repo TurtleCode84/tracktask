c82bfb9b19d6947e33896947744e53d90a30b4cc
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-11-12 10:57:40 -0500

    Big test

--- pages/api/email.js
+++ pages/api/email.js
@@ -4,6 +4,8 @@ import { ObjectId } from "mongodb";
 import clientPromise from "lib/mongodb";
 import parseUuid from "lib/parseUuid";
 import { v1 as uuidv1 } from "uuid";
+import sendEmail from "lib/sendEmail";
+import passwordReset from "templates/passwordReset";
 
 export default withIronSessionApiRoute(emailRoute, sessionOptions);
 
@@ -16,8 +18,24 @@ async function emailRoute(req, res) {
   const client = await clientPromise;
   const db = client.db("data");
   if (req.method === 'POST') { // Sends an email (verification, password reset)
-    res.status(503).json({ message: "Under construction" });
-    return;
+    const { type } = await body;
+    if (!user.email) {
+      res.status(422).json({ message: "You have not linked an email address to your account!" });
+      return;
+    }
+    const uuid = uuidv1();
+    var email;
+    if (type === "verify") {
+      res.status(503).json({ message: "Under construction" });
+      return;
+    } else if (type === "password") {
+      email = passwordReset(user.username, uuid);
+    } else {
+      res.status(422).json({ message: "Invalid data" });
+      return;
+    }
+    const sentMail = await sendEmail(user.email, email.subject, email.html);
+    res.json(sentMail);
   } else if (req.method === 'PATCH') { // Attempts to verify the current user
     const body = await req.body;
     res.status(503).json({ message: "Under construction" });
