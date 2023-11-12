3b44157896d38533bc5642777a2f3c27aa2fde9b
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-11-11 22:26:28 -0500

    unused import

--- pages/terms.js
+++ pages/terms.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Layout from "components/Layout";
 import Link from "next/link";
-import passwordReset from "templates/passwordReset";
 
 export default function Terms() {
   return (
