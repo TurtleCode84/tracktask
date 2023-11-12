4044f1daa78bfeb7abdb17fa3c69dde1b8a5e4a1
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-11-11 22:11:48 -0500

    test

--- templates/passwordReset.js
+++ templates/passwordReset.js
@@ -1,5 +1,13 @@
 export default function passwordReset(username, key) {
   return (
+    <>
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+      <title>TrackTask - Password Reset Requested</title>
+    </head>
+    <body>
         <table cellPadding="0" cellSpacing="0" border="0" width="100%" bgColor="#f4f4f4">
           <tr>
             <td align="center">
@@ -10,13 +18,13 @@ export default function passwordReset(username, key) {
                     <h1>Password Reset Requested</h1>
                     <p>Dear {username},</p>
         
-                    <p>We&apos;ve received a request to reset your password for your account with TrackTask. Your account&apos;s security is our top priority, and we&apos;re here to help you regain access. If you didn&apos;t request this email, someone may be trying to attack your account. Please delete this email if you didn&apos;t request it.</p>
+                    <p>We&apos;ve received a request to reset your password for your account with TrackTask. Your account&apos;s security is our top priority, and we&apos;re here to help you regain access. If you didn&apos;t request this email, someone may be trying to compromise your account. Please delete this email if you didn&apos;t request it.</p>
 
                     <p>To reset your password, please follow the steps below:</p>
 
-                    &bull; Click on the following link to open the password reset page: <a href={`https://tracktask.eu.org/account/reset-password/${key}`}>Reset Your Password</a><br><br>
-                    &bull; Once the page loads, you will be prompted to enter a new password. Please choose a strong, unique password that includes a combination of upper and lower-case letters, numbers, and symbols for added security.<br><br>
-                    &bull; After entering your new password, confirm it by typing it again in the provided field.<br><br>
+                    &bull; Click on the following link to open the password reset page: <a href={`https://tracktask.eu.org/account/reset-password/${key}`}>Reset Your Password</a><br/><br/>
+                    &bull; Once the page loads, you will be prompted to enter a new password. Please choose a strong, unique password that includes a combination of upper and lower-case letters, numbers, and symbols for added security.<br/><br/>
+                    &bull; After entering your new password, confirm it by typing it again in the provided field.<br/><br/>
                     &bull; Click the "Reset Password" button.
 
                     <p>If you did not initiate this password reset request, please disregard this email. Your current password will remain unchanged.</p>
@@ -26,7 +34,7 @@ export default function passwordReset(username, key) {
                     <p>If you encounter any issues or have questions, please don&apos;t hesitate to contact our support team at <a href="mailto:tracktask@tracktask.eu.org">tracktask@tracktask.eu.org</a>.</p>
 
                     <p>Best,</p>
-                    <p>The TrackTask Team<br>
+                    <p>The TrackTask Team<br/>
                     <a href="https://tracktask.eu.org">Visit the website</a></p>
                 
                     <p><em>Remember to keep your new password safe, don&apos;t share it with anyone!</em></p>
@@ -36,5 +44,7 @@ export default function passwordReset(username, key) {
             </td>
           </tr>
         </table>
+    </body>
+    </html></>
   );
 }
