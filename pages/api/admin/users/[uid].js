9bcd87f36bc25c5080b39bf180c894d8c3b2d55a
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-11-19 17:05:22 -0500

    Make sure emails are unique when verified

--- pages/api/admin/users/[uid].js
+++ pages/api/admin/users/[uid].js
@@ -88,6 +88,9 @@ async function adminUserRoute(req, res) {
         $set: {'permissions.verified': body.verify},
       };
       await db.collection("users").updateOne(query, verifyUpdateDoc); // See above
+      // If anyone else has the same email as newly verified user, we need to get rid of it
+      const verifiedUser = await db.collection("users").findOne(query, { projection: { email: 1 } });
+      await db.collection("users").updateMany({ _id: { $ne: verifiedUser._id }, email: verifiedUser.email }, { $set: { email: "", verificationKey: "", otp: "", 'permissions.verified': false } });
     }
     if (body.admin !== undefined) { // true or false
       if (process.env.SUPERADMIN !== user.id || user.id === uid) {
