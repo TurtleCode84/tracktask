7f155a85863b1dc71308df4a7d222a6bbce4e32f
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2023-11-19 21:05:58 -0500

    Fix edge case

--- pages/api/admin/users/[uid].js
+++ pages/api/admin/users/[uid].js
@@ -90,7 +90,7 @@ async function adminUserRoute(req, res) {
       await db.collection("users").updateOne(query, verifyUpdateDoc); // See above
       // If anyone else has the same email as newly verified user, we need to get rid of it
       const verifiedUser = await db.collection("users").findOne(query, { projection: { email: 1 } });
-      await db.collection("users").updateMany({ _id: { $ne: verifiedUser._id }, email: verifiedUser.email }, { $set: { email: "", verificationKey: "", otp: "", 'permissions.verified': false } });
+      await db.collection("users").updateMany({ _id: { $ne: verifiedUser._id }, $and: [ {email: verifiedUser.email}, {email: {$ne: ""}} ]}, { $set: { email: "", verificationKey: "", otp: "", 'permissions.verified': false } });
     }
     if (body.admin !== undefined) { // true or false
       if (process.env.SUPERADMIN !== user.id || user.id === uid) {
