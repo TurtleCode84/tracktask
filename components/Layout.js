4e75cbc801b147b2688922b8b87130e95917ca25
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2024-01-27 17:05:05 -0500

    Increase maximum icon width

--- components/Layout.js
+++ components/Layout.js
@@ -241,7 +241,7 @@ export default function Layout({ children }) {
           filter: brightness(var(--icon-brightness));
           vertical-align: -5px;
           font-size: inherit;
-          max-width: 25px;
+          max-width: 32px;
           overflow: hidden;
           font-variation-settings:
             'FILL' 1,
