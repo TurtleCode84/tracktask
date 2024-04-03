137177e74260e3a5a7ee6ad7d79e6fb5f4b759e5
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2024-04-02 23:39:45 -0400

    Fix states

--- components/Collection.js
+++ components/Collection.js
@@ -7,8 +7,9 @@ export default function Collection({ user, collection, key, admin }) {
   if (collection.owner !== user.id && !admin) {
     sharedColor = "#006dbe";
   }
+  const urlState = {"url":"/dashboard","as":`/dashboard#collection-${collection._id}`,"options":{"locale":"en","scroll":true,"_shouldResolveHref":true},"__N":true,"key":key};
   return (
-    <li key={key} id={"collection-" + collection._id} className="list-hover" style={{ margin: "0.5em", background: "var(--element-background)", padding: "8px", borderWidth: "2px", borderStyle: "solid", borderColor: "var(--border-color)", borderRadius: "10px", width: "auto" }} onClick={() => router.push(`/${admin === true ? 'admin/' : ''}collections/${collection._id}`)}>
+    <li key={key} id={"collection-" + collection._id} className="list-hover" style={{ margin: "0.5em", background: "var(--element-background)", padding: "8px", borderWidth: "2px", borderStyle: "solid", borderColor: "var(--border-color)", borderRadius: "10px", width: "auto" }} onClick={() => {history.pushState(urlState, "", `${window.location.href}#collection-${collection._id}`);router.push(`/${admin === true ? 'admin/' : ''}collections/${collection._id}`);}}>
       {collection.pending ? <span title="Share request" style={{ color: "#006dbe" }} className="material-symbols-outlined icon-list">share_reviews</span> : <>{collection.sharing.shared ? <span title="Shared" style={{ color: sharedColor }} className="material-symbols-outlined icon-list">group</span> : <span title="Private" style={{ color: "lightslategray" }} className="material-symbols-outlined icon-list">lock</span>}</>}{' '}<b>{collection.name}</b>{' '}(created {collection.created > 0 ? moment.unix(collection.created).fromNow() : 'never'})
       <div style={{ marginLeft: "22px", marginTop: "3px", color: "var(--secondary-text-color)" }}>{collection.description.length > 50 ? <>{collection.description.slice(0,50).trim()}...</> : collection.description}</div>
     </li>
