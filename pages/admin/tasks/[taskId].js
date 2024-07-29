e5b1b6bfb3af5303dc4a01075e79831da258e2e8
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2024-07-29 18:11:11 -0400

    fix

--- pages/admin/tasks/[taskId].js
+++ pages/admin/tasks/[taskId].js
@@ -25,8 +25,9 @@ export default function TaskAdmin() {
   
   const [errorMsg, setErrorMsg] = useState("");
 
-  const collectionTags = task?.collections.map((item, index) =>
-    <Link key={index} href={`/admin/collections/${item._id}`}><span style={{fontSize: "18px", verticalAlign: "2px", backgroundColor: stringToColor(item._id), padding: "0.5px 4px", borderStyle: "solid", borderWidth: "2px", borderColor: "var(--inset-border-color)", borderRadius: "7px", color: "#111", marginRight: "6px", display: "inline-block", filter: "grayscale(0.4) brightness(1.5)" }}>{collections.find((collection) => {collection._id === item._id}).name}</span></Link>
+  const filteredCollections = collections?.filter((collection) => {collection?.tasks.includes(task?._id)});
+  const collectionTags = filteredCollections?.map((item, index) =>
+    <Link key={index} href={`/admin/collections/${item._id}`}><span style={{fontSize: "18px", verticalAlign: "2px", backgroundColor: stringToColor(item._id), padding: "0.5px 4px", borderStyle: "solid", borderWidth: "2px", borderColor: "var(--inset-border-color)", borderRadius: "7px", color: "#111", marginRight: "6px", display: "inline-block", filter: "grayscale(0.4) brightness(1.5)" }}>{item.name}</span></Link>
   );
     
   if (!user || !user.isLoggedIn || user.permissions.banned || !user.permissions.admin) {
