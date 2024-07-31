9faa8a1880f7d5f354b5bc056f7ee10aae72b01d
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2024-07-31 12:37:53 -0400

    Patch fix for tracktask-client

--- pages/api/[[...dataPath]].js
+++ pages/api/[[...dataPath]].js
@@ -7,7 +7,7 @@ import moment from "moment";
 export default withIronSessionApiRoute(dataRoute, sessionOptions);
 
 async function dataRoute(req, res) {
-  const user = req.session.user;
+  const user = await req.session.user;
   const { dataPath, filter } = req.query;
   const allowedPaths = ["tasks", "collections"];
 
