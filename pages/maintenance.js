564ca08e686a591abdcec4051a8391670fb0a0d6
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2024-08-02 15:03:19 -0400

    Add status trigger to maintenance mode page

--- pages/maintenance.js
+++ pages/maintenance.js
@@ -6,6 +6,7 @@ export default function Maintenance() {
             <h1 style={{fontSize: 48, marginBottom: "0px"}}>Maintenance in Progress <span style={{color: "#006dbe", fontSize: "inherit", maxWidth: "46px"}} className="material-symbols-outlined">engineering</span></h1>
             <h3>We&apos;re working on improving your user experience, TrackTask will be back online as soon as possible.</h3>
             <p>Thank you for your patience & cooperation!</p>
+            {/*@status:maintenance_mode*/}
         </Layout>
     );
 }
