fcd7be40076fcc6af074c108fc63606f89e4d2b7
TurtleCode <106502718+TurtleCode84@users.noreply.github.com>
2025-03-09 21:57:33 -0400

    Make restriction more obvious

--- components/UserAdminForm.js
+++ components/UserAdminForm.js
@@ -62,13 +62,12 @@ export default function UserAdminForm({ errorMessage, successMessage, onSubmit, 
         <input type="checkbox" name="ban" defaultChecked={lookup.permissions.banned} onChange={(e) => {setCanEditBanReason(e.target.checked)}} />
       </label>
       <label>
-        {canEditBanReason && "true"}
         {lookup.permissions.banned ?
         <><span>Edit ban reason</span>
         <input type="text" defaultValue={lookup.history.ban.reason} name="banReason" disabled={!canEditBanReason} /></>
         :
-        <><span>Ban reason</span>
-        <input type="text" name="banReason" disabled={!canEditBanReason} /></>
+        <>{canEditBanReason && <><span>Ban reason</span>
+        <input type="text" name="banReason" /></>}</>
         }
       </label><hr/>
 
